6b1443155d11cbfd8b11c549f896103bb9def7de
Abhay Raj Singh <abhayraja4@gmail.com>
2021-03-08 09:49:33 +0530

    Solution for cyclic_shift.py MLE

--- hackerearth/codemonk/arrays_and_strings/cyclic_shift.py
+++ hackerearth/codemonk/arrays_and_strings/cyclic_shift.py
@@ -1,15 +1,51 @@
+# returns list of all the values of cyclic shifts where the A is B, i.e. max
+def find_max_shifts(size_of_binary_string, binary_string):
+    all_shifts = []
+    max_shift = binary_string
+    max_shifts = []
+    for shift in range(size_of_binary_string):
+        current_shift = binary_string[size_of_binary_string - shift:] + binary_string[:size_of_binary_string - shift]
+        # print(current_shift)
+        if current_shift > max_shift:
+            max_shift = current_shift
+        all_shifts.append(current_shift)
+    # print('max_shift', max_shift)
+    all_shifts.reverse()
+    all_shifts.insert(0, all_shifts.pop())
+    # print(all_shifts)
+    for shift in range(size_of_binary_string):
+        if all_shifts[shift] == max_shift:
+            max_shifts.append(shift)
+    return max_shifts
+
+
+for T in range(int(input())):
+    N, K = map(int, input().split())
+    A = input()
+    M = find_max_shifts(N, A)
+    # print(M)
+    size_of_max_shifts = len(M)
+    shift_index = (K % size_of_max_shifts) - 1
+    print(M[shift_index] + (N * ((K - 1) // size_of_max_shifts)))
+
 """
 ========
 INPUT:
-2
+4
 5 2
 10101
 6 2
 010101
+6 15
+010101
+5 2
+11010
 ========
 OUTPUT:
 9
 3
+29
+5
 ========
 EXPLANATION:
 For the 1st test case, the value of B is (11010).
@@ -25,4 +61,13 @@ becomes equal to B for the first time.
 After performing additional 2 cyclic shifts the value represented by array A
 becomes B for the second time. Hence, the answer is
 1 + 2 = 3
+"""
+
+"""
+To check cyclic shifts accuracy
+2
+5 2
+12345
+6 2
+123456
 """
