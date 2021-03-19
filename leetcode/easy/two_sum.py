03d46661bbf315cad93661a910ab89024b08fa05
Abhay Raj Singh <abhayraja4@gmail.com>
2021-03-19 10:03:42 +0530

    Use map to fast-search second for target

--- leetcode/easy/two_sum.py
+++ leetcode/easy/two_sum.py
@@ -8,20 +8,23 @@ class Solution:
     def twoSum(numbers: [int], target: int) -> [int]:
         two_sum = [1, 1]  # indexes of the 2 numbers adding up to target
         numbers_length = len(numbers)
-        for first in range(numbers_length):
-            for second in range(first + 1, numbers_length):
-                if numbers[first] + numbers[second] == target:  # target found
-                    two_sum[0] += first
-                    two_sum[1] += second
-                    return two_sum
-                if numbers[first] + numbers[second] > target:  # backtrack
-                    break
+        indexes = dict()
+        for index in range(numbers_length):
+            indexes[numbers[index]] = index
+        for first_index in range(numbers_length):
+            second = target - numbers[first_index]
+            if second in indexes:
+                two_sum[0] += first_index
+                two_sum[1] += indexes[second]
+                break
         return two_sum
 
 
 assert Solution.twoSum(numbers=[2, 7, 11, 15], target=9) == [1, 2]
 assert Solution.twoSum(numbers=[2, 3, 4], target=6) == [1, 3]
 assert Solution.twoSum(numbers=[-1, 0], target=-1) == [1, 2]
+assert Solution.twoSum(numbers=[5, 25, 75], target=100) == [2, 3]
+assert Solution.twoSum(numbers=[0, 0, 3, 4], target=0) == [1, 2]
 
 """
 Example 1:
