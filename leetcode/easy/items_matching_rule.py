dcddef8cab338090063a9e2eaf6c07beaab6958a
Abhay Raj Singh <abhayraja4@gmail.com>
2021-03-26 09:40:32 +0530

    Item match simple rule checks

--- leetcode/easy/items_matching_rule.py
+++ leetcode/easy/items_matching_rule.py
@@ -6,7 +6,15 @@ https://leetcode.com/problems/count-items-matching-a-rule/
 class Solution:
     @staticmethod
     def countMatches(items: [[str]], ruleKey: str, ruleValue: str) -> int:
-        return len(items)
+        matches = 0
+        for item in items:
+            if ruleKey == "type" and ruleValue == item[0]:
+                matches += 1
+            elif ruleKey == "color" and ruleValue == item[1]:
+                matches += 1
+            elif ruleKey == "name" and ruleValue == item[2]:
+                matches += 1
+        return matches
 
 
 assert Solution.countMatches(
