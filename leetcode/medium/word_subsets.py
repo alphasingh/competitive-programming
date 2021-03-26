ef970b3e45201500db41ec073cbacdf544d65691
Abhay Raj Singh <abhayraja4@gmail.com>
2021-03-26 14:21:43 +0530

    Use Counter with brute force

--- leetcode/medium/word_subsets.py
+++ leetcode/medium/word_subsets.py
@@ -2,11 +2,28 @@
 https://leetcode.com/problems/word-subsets/
 """
 
+from collections import Counter
+
 
 class Solution:
     @staticmethod
     def wordSubsets(A: [str], B: [str]) -> [str]:
-        return A + B
+        counter_a = dict()
+        counter_b = dict()
+        universal_a = list()
+        for b in B:
+            counter_b[b] = Counter(b)
+        for a in A:
+            counter_a[a] = Counter(a)
+        for a in counter_a:
+            is_universal = True
+            for b in counter_b:
+                for key in b:
+                    if (key not in counter_a[a]) or (counter_a[a][key] < counter_b[b][key]):
+                        is_universal = False
+            if is_universal:
+                universal_a.append(a)
+        return universal_a
 
 
 assert Solution.wordSubsets(A=["amazon", "apple", "facebook", "google", "leetcode"],
