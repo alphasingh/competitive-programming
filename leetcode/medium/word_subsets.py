ded41fef351e927686d4fdf6e2032c3e5ced7f4b
Abhay Raj Singh <abhayraja4@gmail.com>
2021-03-26 15:22:29 +0530

    Optimize in a single Counter for B

--- leetcode/medium/word_subsets.py
+++ leetcode/medium/word_subsets.py
@@ -7,7 +7,23 @@ from collections import Counter
 
 class Solution:
     @staticmethod
-    def wordSubsets(A: [str], B: [str]) -> [str]:
+    def wordSubsets(A: [str], B: [str]) -> [str]:  # optimized
+        universal_a = list()
+        net_counter_b = dict()
+        for b in B:
+            counter_b = Counter(b)
+            for key in counter_b:
+                if key not in net_counter_b:
+                    net_counter_b[key] = 0
+                net_counter_b[key] = max(net_counter_b[key], counter_b[key])
+        for a in A:
+            counter_a = Counter(a)
+            if not any((key not in counter_a) or (counter_a[key] < net_counter_b[key]) for key in net_counter_b):
+                universal_a.append(a)
+        return universal_a
+
+    @staticmethod
+    def wordSubsets_bruteForce(A: [str], B: [str]) -> [str]:  # brute force
         counter_a = dict()
         counter_b = dict()
         universal_a = list()
@@ -21,6 +37,7 @@ class Solution:
                 for key in b:
                     if (key not in counter_a[a]) or (counter_a[a][key] < counter_b[b][key]):
                         is_universal = False
+                        break
             if is_universal:
                 universal_a.append(a)
         return universal_a
