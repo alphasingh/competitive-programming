e34955229a265e9577fe5f59ca097171bf1964b3
Abhay Raj Singh <abhayraja4@gmail.com>
2021-03-28 07:29:42 +0530

    Use simple observation for # of matches

--- leetcode/easy/count_of_matches_in_tournament.py
+++ leetcode/easy/count_of_matches_in_tournament.py
@@ -6,15 +6,7 @@ https://leetcode.com/problems/count-of-matches-in-tournament/
 class Solution:
     @staticmethod
     def numberOfMatches(n: int) -> int:
-        m = 0
-        while n != 1:
-            m += n // 2
-            if n & 1:  # n is odd
-                n = n // 2 + 1
-            else:  # n is even
-                n = n // 2
-        # print(m)
-        return m
+        return n - 1
 
 
 assert Solution.numberOfMatches(7) is 6
