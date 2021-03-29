96f751795ced23338208c63fee74098aa73eb6a7
Abhay Raj Singh <abhayraja4@gmail.com>
2021-03-29 12:47:24 +0530

    Sorted digits and use digit sequence for uniqueness

--- leetcode/medium/reconstruct_original_digits_from_english.py
+++ leetcode/medium/reconstruct_original_digits_from_english.py
@@ -6,7 +6,11 @@ from collections import Counter
 
 
 class Solution:
-    DIGITS = ('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
+    DIGITS_OUTPUT = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5',
+                     'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+    #           g       x       w       z       t       u       f       s       i       o
+    DIGITS = ('eight', 'six', 'two', 'zero', 'three', 'four', 'five', 'seven', 'nine', 'one')
+    SORTED_DIGITS = ('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine')
     ALPHABETS = 'abcdefghijklmnopqrstuvwxyz'
 
     @staticmethod
@@ -19,7 +23,7 @@ class Solution:
         # print(jumbled_word_dictionary)
         digit_dictionary = {digit: Counter(digit) for digit in Solution.DIGITS}
         # print(digit_dictionary)
-        for digit in digit_dictionary:
+        for digit in Solution.DIGITS:
             current_dictionary = digit_dictionary[digit]
             digit_minimum_count = 50000
             for alphabet in current_dictionary:
@@ -29,31 +33,8 @@ class Solution:
                 for alphabet in current_dictionary:
                     jumbled_word_dictionary[alphabet] -= current_dictionary[alphabet] * digit_minimum_count
             digit_count[digit] = digit_minimum_count
-        for i, digit in enumerate(Solution.DIGITS):
-            output += str(i) * digit_count[digit]
-        print(output)
-        return output
-
-    @staticmethod
-    def originalDigits_brute(jumbled_word: str) -> str:  # brute force
-        digit_count = {digit: 0 for digit in Solution.DIGITS}  # count for 0-9 digits
-        output = ''  # final output
-        jumbled_word_dictionary = {alphabet: 0 for alphabet in Solution.ALPHABETS}
-        for char in jumbled_word:
-            jumbled_word_dictionary[char] += 1
-        # print(jumbled_word_dictionary)
-        digit_dictionary = {digit: Counter(digit) for digit in Solution.DIGITS}
-        # print(digit_dictionary)
-        while any(jumbled_word_dictionary[alphabet] != 0 for alphabet in jumbled_word_dictionary):
-            for digit in digit_dictionary:
-                current_dictionary = digit_dictionary[digit]
-                all_exist = all(jumbled_word_dictionary[ch] >= current_dictionary[ch] for ch in current_dictionary)
-                if all_exist:
-                    digit_count[digit] += 1
-                    for ch in current_dictionary:
-                        jumbled_word_dictionary[ch] -= current_dictionary[ch]
-        for i, digit in enumerate(Solution.DIGITS):
-            output += str(i) * digit_count[digit]
+        for sorted_digit in Solution.SORTED_DIGITS:
+            output += Solution.DIGITS_OUTPUT[sorted_digit] * digit_count[sorted_digit]
         # print(output)
         return output
 
@@ -61,7 +42,7 @@ class Solution:
 # '0'*c[0] + '1'*c[1] + '2'*c[2] + '3'*c[3] + '4'*c[4] + '5'*c[5] + '6'*c[6] + '7'*c[7] + '8'*c[8] + '9'*c[9]
 
 
+assert Solution.originalDigits("eezertwooononnine") == "01129"
 assert Solution.originalDigits("owoztneoer") == "012"
 assert Solution.originalDigits("fviefuro") == "45"
 assert Solution.originalDigits("zeroonetwothreefourfivesixseveneightnine") == "0123456789"
-assert Solution.originalDigits("eezertwooononnine") == "01129"
