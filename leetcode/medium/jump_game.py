a9c26f43ade7f56adfbb495d81ef828f26362738
Abhay Raj Singh <abhayraja4@gmail.com>
2021-03-31 13:08:45 +0530

    Keep jumping to unvisited elements

--- leetcode/medium/jump_game.py
+++ leetcode/medium/jump_game.py
@@ -6,7 +6,20 @@ https://leetcode.com/problems/jump-game/
 class Solution:
     @staticmethod
     def canJump(nums: [int]) -> bool:
-        return len(nums) ^ 1 == 0
+        to_be_visited = {0}  # first visit at the starting index
+        size_nums = len(nums)
+        visited = [False] * size_nums  # to keep track of the visited elements
+        while to_be_visited:
+            current_visit = to_be_visited.pop()
+            visited[current_visit] = True
+            range_start = current_visit + 1
+            range_end = range_start + nums[current_visit]  # until maximum jump
+            if range_end > size_nums:  # exclude impossible jumps
+                range_end = size_nums
+            for index in range(range_start, range_end):
+                if not visited[index]:
+                    to_be_visited.add(index)
+        return visited[-1]  # last index visited
 
 
 assert Solution.canJump(nums=[2, 3, 1, 1, 4]) is True
