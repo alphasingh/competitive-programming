9272f854151211dcf14551135bd65e41f5556ffc
Abhay Raj Singh <abhayraja4@gmail.com>
2021-03-31 13:24:16 +0530

    Improved brute force with extra checks gives TLE

--- leetcode/medium/jump_game.py
+++ leetcode/medium/jump_game.py
@@ -6,19 +6,25 @@ https://leetcode.com/problems/jump-game/
 class Solution:
     @staticmethod
     def canJump(nums: [int]) -> bool:
-        to_be_visited = {0}  # first visit at the starting index
+        to_be_visited = [0]  # first visit at the starting index
         size_nums = len(nums)
         visited = [False] * size_nums  # to keep track of the visited elements
+        max_visit = 0  # maximum visit reached
         while to_be_visited:
             current_visit = to_be_visited.pop()
             visited[current_visit] = True
             range_start = current_visit + 1
             range_end = range_start + nums[current_visit]  # until maximum jump
-            if range_end > size_nums:  # exclude impossible jumps
-                range_end = size_nums
+            if range_end >= size_nums:
+                visited[-1] = True  # last index can be reached
+                break
+            if range_end <= max_visit:
+                continue
+            else:
+                range_start = max_visit
             for index in range(range_start, range_end):
-                if not visited[index]:
-                    to_be_visited.add(index)
+                if not visited[index] and index not in to_be_visited:
+                    to_be_visited.append(index)
         return visited[-1]  # last index visited
 
 
