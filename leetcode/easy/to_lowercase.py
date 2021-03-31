72a77fc7f07cfdfa8f06bd0a77b24d44ede40209
Abhay Raj Singh <abhayraja4@gmail.com>
2021-03-31 12:51:59 +0530

    Manual implementation with ASCII

--- leetcode/easy/to_lowercase.py
+++ leetcode/easy/to_lowercase.py
@@ -6,7 +6,12 @@ https://leetcode.com/problems/to-lower-case/
 class Solution:
     @staticmethod
     def toLowerCase(word: str) -> str:
-        return word.lower()
+        lower = list(word)
+        for index, character in enumerate(word):
+            ordinal_character = ord(character)
+            if 65 <= ordinal_character <= 90:
+                lower[index] = chr(ordinal_character + 32)
+        return "".join(lower)
 
 
 assert Solution.toLowerCase("Hello") == "hello"
