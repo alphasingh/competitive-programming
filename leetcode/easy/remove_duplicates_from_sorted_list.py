a4edf70ba4f3cd0689fa9d6bb1de71b385abdcbc
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-01 09:49:38 +0530

    Simple iteration in linked list

--- leetcode/easy/remove_duplicates_from_sorted_list.py
+++ leetcode/easy/remove_duplicates_from_sorted_list.py
@@ -3,17 +3,43 @@ https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 """
 
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        return str(self.val) + "->" + str(self.next)
+
+
 class Solution:
     @staticmethod
-    def deleteDuplicates(head: [int]) -> [int]:
-        uniques = list()
-        start = -101
-        for element in head:
-            if element > start:
-                start = element
-                uniques.append(element)
-        return uniques
+    def deleteDuplicates(head: ListNode) -> ListNode:
+        first, second = head, head.next if head else None
+        while second:  # if second exists
+            if second.val == first.val:
+                second = second.next
+                first.next = second
+            else:  # current_node.val <= start
+                first = second
+                second = second.next
+        return head
 
 
-assert Solution.deleteDuplicates(head=[1, 1, 2]) == [1, 2]
-assert Solution.deleteDuplicates(head=[1, 1, 2, 3, 3]) == [1, 2, 3]
+assert str(Solution.deleteDuplicates(ListNode(
+    val=1, next=ListNode(
+        val=2, next=ListNode(
+            val=3, next=None))))) == "1->2->3->None"
+
+listNode = ListNode(
+    val=1, next=ListNode(
+        val=1, next=ListNode(
+            val=2, next=None)))
+assert str(Solution.deleteDuplicates(head=listNode)) == "1->2->None"
+listNode = ListNode(
+    val=1, next=ListNode(
+        val=1, next=ListNode(
+            val=2, next=ListNode(
+                val=3, next=ListNode(
+                    val=3, next=None)))))
+assert str(Solution.deleteDuplicates(head=listNode)) == "1->2->3->None"
