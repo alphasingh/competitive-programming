e493ae87b882efff0ce7cf1cde66d0c7225032f4
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-01 09:08:32 +0530

    Use list and keep storing uniques already sorted

--- leetcode/easy/remove_duplicates_from_sorted_list.py
+++ leetcode/easy/remove_duplicates_from_sorted_list.py
@@ -6,7 +6,13 @@ https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 class Solution:
     @staticmethod
     def deleteDuplicates(head: [int]) -> [int]:
-        return head
+        uniques = list()
+        start = -101
+        for element in head:
+            if element > start:
+                start = element
+                uniques.append(element)
+        return uniques
 
 
 assert Solution.deleteDuplicates(head=[1, 1, 2]) == [1, 2]
