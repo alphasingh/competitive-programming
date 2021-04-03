49b199ed352f4fb7e196457a52de1c88b13381fb
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-03 10:01:02 +0530

    Add methods for linked list

--- leetcode/medium/remove_duplicates_from_sorted_list.py
+++ leetcode/medium/remove_duplicates_from_sorted_list.py
@@ -16,8 +16,22 @@ class ListNode:
 
 class Solution:
     @staticmethod
+    def convert_to_listNode(flat_list) -> ListNode:
+        converted = ListNode()
+        pointer = converted
+        while flat_list:
+            pointer.next = ListNode(flat_list.pop(0))
+            pointer = pointer.next
+        return converted.next
+
+    @staticmethod
     def deleteDuplicates(head: ListNode) -> ListNode:
-        return head
+        flat_list = []
+        while head:
+            flat_list.append(head.val)
+            head = head.next
+        one_timers = Solution.deleteDuplicates_list(flat_list)
+        return Solution.convert_to_listNode(one_timers)
 
     @staticmethod
     def deleteDuplicates_list(head: [int]) -> [int]:
@@ -31,3 +45,6 @@ class Solution:
 
 assert Solution.deleteDuplicates_list(head=[1, 2, 3, 3, 4, 4, 5]) == [1, 2, 5]
 assert Solution.deleteDuplicates_list(head=[1, 1, 1, 2, 3]) == [2, 3]
+
+input_list = ListNode(1, ListNode(2, ListNode(3, ListNode(3, ListNode(4, ListNode(4, ListNode(5)))))))
+assert str(Solution.deleteDuplicates(head=input_list)) == '1->2->5->None'
