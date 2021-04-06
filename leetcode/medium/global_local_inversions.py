45e2372e1cf1b24c11dd5a1678015f3c6d04a73d
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-06 10:29:16 +0530

    Use brute force to pass TCs

--- leetcode/medium/global_local_inversions.py
+++ leetcode/medium/global_local_inversions.py
@@ -7,10 +7,19 @@ class Solution:
 
     @staticmethod
     def isIdealPermutation(A: [int]) -> bool:
-        return len(A) ^ 1 == 0
+        local_inversions = global_inversions = 0
+        size = len(A)
+        for i in range(size):
+            for j in range(i + 1, size):
+                if A[i] > A[j]:
+                    global_inversions += 1
+        for i in range(size - 1):
+            if A[i] > A[i + 1]:
+                local_inversions += 1
+        return local_inversions == global_inversions
 
 
 assert Solution.isIdealPermutation(A=[1, 0, 2]) is True
 # Explanation: There is 1 global inversion, and 1 local inversion.
-assert Solution.isIdealPermutation(A=[1, 2, 0]) is True
+assert Solution.isIdealPermutation(A=[1, 2, 0]) is False
 # Explanation: There are 2 global inversions, and 1 local inversion.
