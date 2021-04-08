3880884d83bb59b106c29ed6de422b6e7c6eb50e
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-08 21:09:20 +0530

    Brute force to make all possible lines and check points

--- leetcode/hard/max_points_on_a_line.py
+++ leetcode/hard/max_points_on_a_line.py
@@ -7,8 +7,15 @@ class Solution:
 
     @staticmethod
     def maxPoints(points: [[int]]) -> int:
-        print(points)
-        return len(points)
+        max_points = 0  # maximum number of points that lie on the same straight line
+        for line_start in points:
+            for line_end in points:
+                points_on_current_line = 0
+                for point in points:
+                    if point:  # if point lies on the current line
+                        points_on_current_line += 1
+                max_points = max(max_points, points_on_current_line)
+        return max_points
 
 
 assert Solution.maxPoints(points=[[1, 1], [2, 2], [3, 3]]) == 3
