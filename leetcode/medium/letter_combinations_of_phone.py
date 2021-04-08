b5aeda14905bba357d0e8a6496a049b1b483a387
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-08 15:31:57 +0530

    Use KEYS to store the mapping

--- leetcode/medium/letter_combinations_of_phone.py
+++ leetcode/medium/letter_combinations_of_phone.py
@@ -4,10 +4,12 @@ https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
 
 class Solution:
+    KEYS = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
 
     @staticmethod
     def letterCombinations(digits: str) -> [str]:
-        return list(digits)
+        combinations = []
+        return combinations
 
 
 assert Solution.letterCombinations("23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
