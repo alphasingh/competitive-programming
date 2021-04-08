7908582be9b6f98e76f72962a137f116d99ad5ff
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-08 21:26:58 +0530

    Add one more TC for failing

--- leetcode/hard/max_points_on_a_line.py
+++ leetcode/hard/max_points_on_a_line.py
@@ -20,9 +20,10 @@ class Solution:
                         if point[0] == line_end[0]:  # if x lies on the vertical line
                             points_on_current_line += 1
                 else:
-                    m = y_diff / x_diff  # slope
+                    m = float(y_diff / x_diff)  # slope
                     for point in points:
-                        if (point[1] - line_end[1]) == m * (point[0] - line_end[0]):
+                        if (point[1] - line_end[1]) == float(m * (point[0] - line_end[0])) or (
+                                point[1] - line_start[1]) == float(m * (point[0] - line_start[0])):
                             points_on_current_line += 1
                 max_points = max(max_points, points_on_current_line)
         # print(max_points)
@@ -32,3 +33,8 @@ class Solution:
 assert Solution.maxPoints(points=[[1, 1], [2, 2], [3, 3]]) == 3
 assert Solution.maxPoints(points=[[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]) == 4
 assert Solution.maxPoints(points=[[0, 0]]) == 1
+assert Solution.maxPoints(
+    [[54, 153], [1, 3], [0, -72], [-3, 3], [12, -22], [-60, -322], [0, -5], [-5, 1], [5, 5], [36, 78], [3, -4], [5, 0],
+     [0, 4], [-30, -197], [-5, 0], [60, 178], [0, 0], [30, 53], [24, 28], [4, 5], [2, -2], [-18, -147], [-24, -172],
+     [-36, -222], [-42, -247], [2, 3], [-12, -122], [-54, -297], [6, -47], [-5, 3], [-48, -272], [-4, -2], [3, -2],
+     [0, 2], [48, 128], [4, 3], [2, 4]]) == 18
