d95a675ad79603b367866ff4ca0bb32b46030b10
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-08 15:55:21 +0530

    Use brute force and keep appending combinations

--- leetcode/medium/letter_combinations_of_phone.py
+++ leetcode/medium/letter_combinations_of_phone.py
@@ -9,9 +9,20 @@ class Solution:
     @staticmethod
     def letterCombinations(digits: str) -> [str]:
         combinations = []
+        for digit in digits:
+            if len(combinations) == 0:
+                combinations = list(Solution.KEYS[digit])
+            else:
+                current_combinations = []
+                for combination in combinations:
+                    for letter in Solution.KEYS[digit]:
+                        current_combinations.append(combination + letter)
+                combinations = current_combinations.copy()
+        # print(combinations)
         return combinations
 
 
 assert Solution.letterCombinations("23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 assert Solution.letterCombinations("") == []
 assert Solution.letterCombinations("2") == ["a", "b", "c"]
+assert Solution.letterCombinations("78") == ["pt", "pu", "pv", "qt", "qu", "qv", "rt", "ru", "rv", "st", "su", "sv"]
