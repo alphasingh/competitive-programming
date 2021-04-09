19aa2c82662ce72e71650b5a4c12f88d12dae9fe
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-09 14:37:19 +0530

    953. Verifying an Alien Dictionary with failing TCs

---
+++ leetcode/easy/alien_dictionary.py
@@ -0,0 +1,22 @@
+"""
+https://leetcode.com/problems/verifying-an-alien-dictionary/
+"""
+
+
+class Solution:
+
+    @staticmethod
+    def isAlienSorted(words: [str], order: str) -> bool:
+        return len(words) <= len(order)
+
+
+assert Solution.isAlienSorted(words=["hello", "leetcode"], order="hlabcdefgijkmnopqrstuvwxyz") is True
+# Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+assert Solution.isAlienSorted(words=["word", "world", "row"], order="worldabcefghijkmnpqstuvxyz") is False
+# Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+assert Solution.isAlienSorted(words=["apple", "app"], order="abcdefghijklmnopqrstuvwxyz") is False
+"""
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) 
+According to lexicographical rules "apple" > "app", because 'l' > '∅', 
+where '∅' is defined as the blank character which is less than any other character (More info).
+"""
