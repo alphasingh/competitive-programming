dcf1584ba2625240a3d21cfffaa7c700cf41561d
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-10 10:48:47 +0530

    Use dp to keep track of palindrome substrings

--- leetcode/medium/longest_palindromic_substring.py
+++ leetcode/medium/longest_palindromic_substring.py
@@ -7,18 +7,33 @@ class Solution:
 
     @staticmethod
     def longestPalindrome(s: str) -> str:
-        max_length = 0
-        longest_palindrome = ""
+        max_length = 1
+        longest_palindrome = s[0]
         length = len(s)
+        is_palindrome = [[0 for _ in range(length)] for _ in range(length)]
         for start in range(length):
-            for end in range(start + 1, length + 1):
-                substring = s[start:end]
-                if substring == substring[::-1] and end - start > max_length:  # is palindrome
-                    max_length = end - start
-                    longest_palindrome = substring
+            is_palindrome[start][start] = 1  # single alphabet is palindrome
+        for start in range(length - 1):
+            if s[start] == s[start + 1]:  # is palindrome
+                is_palindrome[start][start + 1] = 1
+                if max_length < 2:
+                    max_length = 2
+                    longest_palindrome = s[start:start + 2]
+        # print(is_palindrome)
+        for size_of_substring in range(2, length):
+            for start in range(length):
+                end = start + size_of_substring
+                if end < length:
+                    if s[start] == s[end] and is_palindrome[start + 1][end - 1]:  # is palindrome
+                        is_palindrome[start][end] = 1
+                        if end - start + 1 > max_length:
+                            max_length = end - start + 1
+                            longest_palindrome = s[start:end + 1]
+        # print(longest_palindrome, max_length)
         return longest_palindrome
 
 
+assert Solution.longestPalindrome("eptcrhhlhrytwkytqmqlnvgoogjdlejnslpevrtxplzyzlvcy") == "lzyzl"
 assert Solution.longestPalindrome("babad") == "bab"
 # Note: "aba" is also a valid answer.
 assert Solution.longestPalindrome("cbbd") == "bb"
