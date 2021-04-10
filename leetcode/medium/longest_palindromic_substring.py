579b252999134c0b7d0e39cb885ccdac41ed654b
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-10 11:18:17 +0530

    Optimize the range to avoid condition check

--- leetcode/medium/longest_palindromic_substring.py
+++ leetcode/medium/longest_palindromic_substring.py
@@ -21,14 +21,13 @@ class Solution:
                     longest_palindrome = s[start:start + 2]
         # print(is_palindrome)
         for size_of_substring in range(2, length):
-            for start in range(length):
+            for start in range(length - size_of_substring):
                 end = start + size_of_substring
-                if end < length:
-                    if s[start] == s[end] and is_palindrome[start + 1][end - 1]:  # is palindrome
-                        is_palindrome[start][end] = 1
-                        if end - start + 1 > max_length:
-                            max_length = end - start + 1
-                            longest_palindrome = s[start:end + 1]
+                if s[start] == s[end] and is_palindrome[start + 1][end - 1]:  # is palindrome
+                    is_palindrome[start][end] = 1
+                    if end - start + 1 > max_length:
+                        max_length = end - start + 1
+                        longest_palindrome = s[start:end + 1]
         # print(longest_palindrome, max_length)
         return longest_palindrome
 
