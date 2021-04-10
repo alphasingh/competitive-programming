3909001fdcf6be45c363d2bc7e05e4db38ed472b
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-10 20:51:55 +0530

    AC: passed all TCs by google codejam

--- google/codejam/reversort.py
+++ google/codejam/reversort.py
@@ -5,14 +5,21 @@ https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/0000000
 
 def reversort(integers: [int], size: int) -> int:
     cost = 0
-    print(integers, size)
+    # print(integers, size)
+    for position in range(size - 1):
+        minimum_element = min(integers[position:])
+        minimum_element_index = integers.index(minimum_element)
+        reversed_integers = integers[position: minimum_element_index + 1][::-1]
+        integers = integers[:position] + reversed_integers + integers[minimum_element_index + 1:]
+        cost += minimum_element_index - position + 1
+        # print(integers)
     return cost
 
 
 assert reversort(integers=[4, 2, 1, 3], size=4) == 6
 assert reversort(integers=[1, 2], size=2) == 1
 assert reversort(integers=[7, 6, 5, 4, 3, 2, 1], size=7) == 12
-assert int(True) == 0  # change to 1 to switch on custom TCs
+assert int(True) == 1  # change to 1 to switch on custom TCs
 
 for T in range(int(input())):
     N = int(input())
