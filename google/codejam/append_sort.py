5d4d8168d2a4574c5a8d2440dd05c4fc43d17b5f
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-10 14:06:05 +0530

    Practice append sort with failing TCs

---
+++ google/codejam/append_sort.py
@@ -0,0 +1,36 @@
+"""
+https://codingcompetitions.withgoogle.com/codejam/round/000000000043585d/00000000007549e5
+"""
+
+
+def minimum_operations(integers: [str], size: int) -> int:
+    return len(integers) + size
+
+
+assert minimum_operations(integers=[100, 7, 10], size=3) == 4
+assert minimum_operations(integers=[10, 10], size=2) == 1
+assert minimum_operations(integers=[4, 19, 1], size=3) == 2
+assert minimum_operations(integers=[1, 2, 3], size=3) == 0
+
+for T in range(int(input())):
+    N = int(input())
+    X = list(map(int, input().split()))
+    print("Case #{}: {}".format(T + 1, minimum_operations(X, N)))
+
+'''
+INPUT:
+4
+3
+100 7 10
+2
+10 10
+3
+4 19 1
+3
+1 2 3
+OUTPUT:
+Case #1: 4
+Case #2: 1
+Case #3: 2
+Case #4: 0
+'''
