0baef360b9aa4d1f36f1becdf2e09a5714af8cf3
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-10 14:55:46 +0530

    Add switch to check custom TCs as given by Google

--- google/codejam/append_sort.py
+++ google/codejam/append_sort.py
@@ -17,6 +17,7 @@ def minimum_operations(integers: [int], size: int) -> int:
         # print(first, second)
         # print(integers)
         integers[i + 1] = second
+    print(integers)
     # print(operations)
     return operations
 
@@ -25,6 +26,7 @@ assert minimum_operations(integers=[100, 7, 10], size=3) == 4
 assert minimum_operations(integers=[10, 10], size=2) == 1
 assert minimum_operations(integers=[4, 19, 1], size=3) == 2
 assert minimum_operations(integers=[1, 2, 3], size=3) == 0
+assert int(True) == 0  # change to 1 to switch on custom TCs
 
 for T in range(int(input())):
     N = int(input())
