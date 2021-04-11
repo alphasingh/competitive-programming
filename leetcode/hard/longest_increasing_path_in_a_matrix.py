8ef549f3a11cc170321bdc12f7861fde161a0a97
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-11 09:35:57 +0530

    Fix bug with an additional TC by leetcode

--- leetcode/hard/longest_increasing_path_in_a_matrix.py
+++ leetcode/hard/longest_increasing_path_in_a_matrix.py
@@ -12,7 +12,7 @@ class Solution:
             dfs_up = dfs(row - 1, col) if row and val < matrix[row - 1][col] else 0
             dfs_down = dfs(row + 1, col) if row + 1 < m and val < matrix[row + 1][col] else 0
             dfs_left = dfs(row, col - 1) if col and val < matrix[row][col - 1] else 0
-            dfs_right = dfs(row, col + 1) if col + 1 < m and val < matrix[row][col + 1] else 0
+            dfs_right = dfs(row, col + 1) if col + 1 < n and val < matrix[row][col + 1] else 0
             dp[row][col] = 1 + max(dfs_up, dfs_down, dfs_left, dfs_right)
             return dp[row][col]
 
@@ -30,3 +30,4 @@ assert Solution.longestIncreasingPath(matrix=[[3, 4, 5], [3, 2, 6], [2, 2, 1]]) 
 Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
 """
 assert Solution.longestIncreasingPath(matrix=[[1]]) == 1
+assert Solution.longestIncreasingPath(matrix=[[1, 2]]) == 2
