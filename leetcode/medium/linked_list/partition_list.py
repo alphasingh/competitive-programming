5eaf9e0e91ea02c436c494e3857f8a7615d012ad
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-14 22:47:33 +0530

    Assert different out1 for head1

--- leetcode/medium/linked_list/partition_list.py
+++ leetcode/medium/linked_list/partition_list.py
@@ -17,7 +17,8 @@ class Solution:
 
 
 head1 = ListNode(1, next=ListNode(4, next=ListNode(3, next=ListNode(2, next=ListNode(5, next=ListNode(2))))))
-assert Solution.partition(head=head1, x=3) == head1
+out1 = ListNode(1, next=ListNode(2, next=ListNode(2, next=ListNode(4, next=ListNode(3, next=ListNode(5))))))
+assert Solution.partition(head=head1, x=3) == out1
 """
 Input: head = [1,4,3,2,5,2], x = 3
 Output: [1,2,2,4,3,5]
