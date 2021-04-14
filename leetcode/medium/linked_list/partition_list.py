8ecfa4ad1dc7e455e563a624217e14c975b6753f
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-14 23:45:12 +0530

    Compare strings for assertion as memory reference

--- leetcode/medium/linked_list/partition_list.py
+++ leetcode/medium/linked_list/partition_list.py
@@ -9,23 +9,56 @@ class ListNode:
         self.val = val
         self.next = next
 
+    def __str__(self):
+        return str(self.val) + "->" + str(self.next)
+
 
 class Solution:
     @staticmethod
+    def list_to_node(array: [int]) -> ListNode:
+        result = None
+        size = len(array)
+        if size == 0:
+            return result
+        elif size == 1:
+            return ListNode(array[0])
+        result = ListNode()
+        pointer = result
+        for index in range(len(array) - 1):
+            pointer.val = array[index]
+            pointer.next = ListNode(array[index + 1])
+            pointer = pointer.next
+        return result
+
+    @staticmethod
     def partition(head: ListNode, x: int) -> ListNode:
-        return head
+        lower = []
+        higher = []
+        current_node = head
+        # create lower and higher
+        while current_node:
+            value = current_node.val
+            if value < x:
+                lower.append(value)
+            else:
+                higher.append(value)
+            current_node = current_node.next
+        # merge lower and higher
+        merged = lower + higher
+        # print(merged)
+        return Solution.list_to_node(merged)
 
 
 head1 = ListNode(1, next=ListNode(4, next=ListNode(3, next=ListNode(2, next=ListNode(5, next=ListNode(2))))))
 out1 = ListNode(1, next=ListNode(2, next=ListNode(2, next=ListNode(4, next=ListNode(3, next=ListNode(5))))))
-assert Solution.partition(head=head1, x=3) == out1
+assert str(Solution.partition(head=head1, x=3)) == str(out1)
 """
 Input: head = [1,4,3,2,5,2], x = 3
 Output: [1,2,2,4,3,5]
 """
 head2 = ListNode(2, next=ListNode(1))
 out2 = ListNode(1, next=ListNode(2))
-assert Solution.partition(head=head2, x=2) == out2
+assert str(Solution.partition(head=head2, x=2)) == str(out2)
 """
 Input: head = [2,1], x = 2
 Output: [1,2]
