1a2533e2c66677908159055f1400a733a6d8fd2b
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-14 22:44:58 +0530

    86. Partition List with failing TCs

---
+++ leetcode/medium/linked_list/partition_list.py
@@ -0,0 +1,31 @@
+"""
+https://leetcode.com/problems/partition-list/
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    @staticmethod
+    def partition(head: ListNode, x: int) -> ListNode:
+        return head
+
+
+head1 = ListNode(1, next=ListNode(4, next=ListNode(3, next=ListNode(2, next=ListNode(5, next=ListNode(2))))))
+assert Solution.partition(head=head1, x=3) == head1
+"""
+Input: head = [1,4,3,2,5,2], x = 3
+Output: [1,2,2,4,3,5]
+"""
+head2 = ListNode(2, next=ListNode(1))
+out2 = ListNode(1, next=ListNode(2))
+assert Solution.partition(head=head2, x=2) == out2
+"""
+Input: head = [2,1], x = 2
+Output: [1,2]
+"""
