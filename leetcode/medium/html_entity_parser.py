90725c7225a7b55567674023537d810d7ee2a30f
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-15 09:14:47 +0530

    1410. HTML Entity Parser use regex

---
+++ leetcode/medium/html_entity_parser.py
@@ -0,0 +1,41 @@
+"""
+https://leetcode.com/problems/html-entity-parser/
+"""
+
+import re
+
+
+class Solution:
+
+    @staticmethod
+    def entityParser(text: str) -> str:
+        entities = {"&quot;": "\"", "&apos;": "\'", "&gt;": ">", "&lt;": "<", "&frasl;": "/", "&amp;": "&"}
+        for entity in entities:
+            text = re.sub(rf"{entity}", entities[entity], text)
+        return text
+
+
+input_html = "&amp;gt;"
+output_html = "&gt;"
+assert Solution.entityParser(input_html) == output_html
+
+input_html = "&amp; is an HTML entity but &ambassador; is not."
+output_html = "& is an HTML entity but &ambassador; is not."
+assert Solution.entityParser(input_html) == output_html
+"""Explanation: The parser will replace the &amp; entity by &"""
+
+input_html = "and I quote: &quot;...&quot;"
+output_html = "and I quote: \"...\""
+assert Solution.entityParser(input_html) == output_html
+
+input_html = "Stay home! Practice on Leetcode :)"
+output_html = "Stay home! Practice on Leetcode :)"
+assert Solution.entityParser(input_html) == output_html
+
+input_html = "x &gt; y &amp;&amp; x &lt; y is always false"
+output_html = "x > y && x < y is always false"
+assert Solution.entityParser(input_html) == output_html
+
+input_html = "leetcode.com&frasl;problemset&frasl;all"
+output_html = "leetcode.com/problemset/all"
+assert Solution.entityParser(input_html) == output_html
