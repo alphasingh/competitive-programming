a289eb1252aa370a0491edeaf8bbdecf8afc6d69
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-16 08:54:14 +0530

    Optimize a break rather than finding all elements

--- leetcode/medium/minimum_rotated_sorted.py
+++ leetcode/medium/minimum_rotated_sorted.py
@@ -7,9 +7,15 @@ class Solution:
 
     @staticmethod
     def findMin(nums: [int]) -> int:
-        return min(nums)
+        minimum = nums[0]
+        for i in range(len(nums) - 1):
+            if nums[i + 1] < nums[i]:
+                minimum = nums[i + 1]
+                break
+        return minimum
 
 
 assert Solution.findMin([3, 4, 5, 1, 2]) == 1
 assert Solution.findMin([4, 5, 6, 7, 0, 1, 2]) == 0
 assert Solution.findMin([11, 13, 15, 17]) == 11
+assert Solution.findMin([-200]) == -200
