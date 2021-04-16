d1cd564b0dedb11e2c100ff0a80268d1add762f8
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-16 20:12:53 +0530

    Brute force for iterations until removals done

--- leetcode/medium/remove_adjacent_duplicates.py
+++ leetcode/medium/remove_adjacent_duplicates.py
@@ -7,7 +7,20 @@ class Solution:
 
     @staticmethod
     def removeDuplicates(s: str, k: int) -> str:
-        return s + str(k)
+        result = s
+        removal_start = 0
+        # print("k:", k)
+        length_of_result = len(result)
+        while removal_start <= length_of_result - k:
+            to_be_removed = result[removal_start:removal_start + k]
+            # print(result, length_of_result, to_be_removed)
+            if len(to_be_removed) == k and len(set(to_be_removed)) == 1:  # k adjacent duplicates
+                length_of_result -= k
+                result = result[:removal_start] + result[removal_start + k:]
+                removal_start = 0
+            else:
+                removal_start += 1
+        return result
 
 
 assert Solution.removeDuplicates(s="abcd", k=2) == "abcd"
