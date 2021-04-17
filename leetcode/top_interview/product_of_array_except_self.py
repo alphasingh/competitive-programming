cf91676b8dddc1505b18d32d6d996450befb8f65
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-17 12:02:12 +0530

    Brute force with O(n) and pivot logic for 0

--- leetcode/top_interview/product_of_array_except_self.py
+++ leetcode/top_interview/product_of_array_except_self.py
@@ -6,7 +6,22 @@ https://leetcode.com/problems/battleships-in-a-board/
 class Solution:
     @staticmethod
     def productExceptSelf(nums: [int]) -> [int]:
-        return len(nums)
+        product = 1
+        pivot = 0
+        if nums.count(0) > 0:
+            pivot = nums.index(0)
+        for i in range(len(nums)):
+            if i != pivot:
+                product *= nums[i]
+        # print(product, pivot)
+        product_with_pivot = product * nums[pivot]
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                nums[i] = product
+            else:
+                nums[i] = product_with_pivot // nums[i]
+        # print(nums)
+        return nums
 
 
 assert Solution.productExceptSelf(nums=[1, 2, 3, 4]) == [24, 12, 8, 6]
