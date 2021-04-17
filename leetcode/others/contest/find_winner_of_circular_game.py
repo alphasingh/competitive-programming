2f5ef5e8e39851b8532e05e772f907c02cd819c5
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-17 08:06:36 +0530

    Simulate the game and return last friend remaining

--- leetcode/others/contest/find_winner_of_circular_game.py
+++ leetcode/others/contest/find_winner_of_circular_game.py
@@ -6,8 +6,15 @@ https://leetcode.com/contest/weekly-contest-236/problems/find-the-winner-of-the-
 class Solution:
     @staticmethod
     def findTheWinner(n: int, k: int) -> int:
-        winner = 0
-        return winner + 1
+        friends = [friend for friend in range(1, n + 1)]
+        remaining_friends = n
+        next_removal = 0
+        while remaining_friends != 1:
+            next_removal = (next_removal + k - 1) % remaining_friends
+            # print(friends, next_removal)
+            friends.pop(next_removal)
+            remaining_friends -= 1
+        return friends[0]  # last friend
 
 
 assert Solution.findTheWinner(n=5, k=2) == 3
@@ -23,7 +30,7 @@ Explanation: Here are the steps of the game:
 8) Count 2 friends clockwise, which are friends 3 and 5.
 9) Friend 5 leaves the circle. Only friend 3 is left, so they are the winner.
 """
-assert Solution.findTheWinner(n=5, k=2) == 1
+assert Solution.findTheWinner(n=6, k=5) == 1
 """
 Explanation: The friends leave in this order: 5, 4, 6, 2, 3. The winner is friend 1.
 """
