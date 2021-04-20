5bdaadbb18d0ee11753f04c44a734f2a6017418a
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-20 14:12:56 +0530

    Use simple math and factorial

--- leetcode/top_interview/62_unique_paths.py
+++ leetcode/top_interview/62_unique_paths.py
@@ -4,14 +4,28 @@ https://leetcode.com/problems/unique-paths/
 
 
 class Solution:
+    factorial = [1, 1]
 
-    @staticmethod
-    def uniquePaths(m: int, n: int) -> int:
-        unique_paths = 1
+    def fact(self, number: int) -> int:
+        if number > len(self.factorial) - 1:
+            self.factorial.append(number * self.fact(number - 1))
+        return self.factorial[number]
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        m, n = m - 1, n - 1
+        m, n = max(m, n), min(m, n)
+        numerator = 1
+        for i in range(m + 1, m + n + 1):
+            numerator *= i
+        denominator = self.fact(n)
+        # print(numerator, denominator)
+        unique_paths = numerator // denominator
+        # print(self.factorial, unique_paths)
         return unique_paths
 
 
-assert Solution.uniquePaths(m=3, n=2) == 3
+sol = Solution()
+assert sol.uniquePaths(m=3, n=2) == 3
 """
 Explanation:
 From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
@@ -19,6 +33,6 @@ From the top-left corner, there are a total of 3 ways to reach the bottom-right 
 2. Down -> Down -> Right
 3. Down -> Right -> Down
 """
-assert Solution.uniquePaths(m=3, n=7) == 28
-assert Solution.uniquePaths(m=7, n=3) == 28
-assert Solution.uniquePaths(m=3, n=3) == 6
+assert sol.uniquePaths(m=3, n=7) == 28
+assert sol.uniquePaths(m=7, n=3) == 28
+assert sol.uniquePaths(m=3, n=3) == 6
