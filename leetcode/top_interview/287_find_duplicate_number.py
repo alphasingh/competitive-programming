3bbd73043d6f0edb8e146c686f9bf1dd3dfd426c
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-20 12:21:57 +0530

    Use counter to keep check on frequency

--- leetcode/top_interview/287_find_duplicate_number.py
+++ leetcode/top_interview/287_find_duplicate_number.py
@@ -6,9 +6,16 @@ https://leetcode.com/problems/find-the-duplicate-number/
 class Solution:
 
     def findDuplicate(self, nums: [int]) -> int:
-        n = len(nums) - 1
-        sum_of_n_numbers = n * (n + 1) // 2
-        return sum(nums) - sum_of_n_numbers
+        counter = [0 for _ in nums]
+        for num in nums:
+            counter[num - 1] += 1
+        # print(counter)
+        duplicate = 1
+        for num in nums:
+            if counter[num - 1] > 1:
+                duplicate = num
+                break
+        return duplicate
 
 
 sol = Solution()
@@ -17,4 +24,5 @@ assert sol.findDuplicate(nums=[1, 3, 4, 2, 2]) == 2
 assert sol.findDuplicate(nums=[3, 1, 3, 4, 2]) == 3
 assert sol.findDuplicate(nums=[1, 1]) == 1
 assert sol.findDuplicate(nums=[1, 1, 2]) == 1
-assert sol.findDuplicate(nums=[1, 3, 3]) == 3
+assert sol.findDuplicate(nums=[2, 2, 2]) == 2
+assert sol.findDuplicate(nums=[1, 3, 4, 4, 4]) == 4
