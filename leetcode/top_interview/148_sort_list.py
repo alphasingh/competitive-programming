35f23ffc0955088091742be2b78ba727555ed0cd
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-21 14:14:44 +0530

    Use O(n) space and sort list in O(nlogn) time

--- leetcode/top_interview/148_sort_list.py
+++ leetcode/top_interview/148_sort_list.py
@@ -9,12 +9,21 @@ class ListNode:
         self.val = val
         self.next = next
 
+    def __str__(self):
+        return str(self.val) + '>' + str(self.next)
+
 
 class Solution:
 
     @staticmethod
     def convert_to_linked_list(array: [int]) -> ListNode:
-        return
+        head = ListNode(array[0])
+        current = head
+        for i in range(len(array) - 1):
+            current.next = ListNode(array[i + 1])
+            current = current.next
+        # print(head)
+        return head
 
     @staticmethod
     def sortList(head: ListNode = None) -> ListNode:
@@ -25,10 +34,10 @@ class Solution:
         while current:
             array.append(current.val)
             current = current.next
-        print(array, 'sorted', sorted(array))
-        return head
+        # print(array, 'sorted', sorted(array))
+        return Solution.convert_to_linked_list(sorted(array))
 
 
-assert Solution.sortList(ListNode(4, ListNode(2, ListNode(1, ListNode(3)))))
-assert Solution.sortList(ListNode(-1, ListNode(5, ListNode(3, ListNode(4, ListNode(0))))))
+assert str(Solution.sortList(ListNode(4, ListNode(2, ListNode(1, ListNode(3)))))) == '1>2>3>4>None'
+assert str(Solution.sortList(ListNode(-1, ListNode(5, ListNode(3, ListNode(4, ListNode(0))))))) == '-1>0>3>4>5>None'
 assert Solution.sortList(None) is None
