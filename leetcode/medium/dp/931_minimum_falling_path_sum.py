f308b088e4f6c7f50b1bf50753260cc05253390d
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-21 20:24:09 +0530

    931. Minimum Falling Path Sum failing TCs

---
+++ leetcode/medium/dp/931_minimum_falling_path_sum.py
@@ -0,0 +1,26 @@
+"""
+https://leetcode.com/problems/minimum-falling-path-sum/
+"""
+
+
+class Solution:
+    @staticmethod
+    def minFallingPathSum(matrix: [[int]]) -> int:  # bottom-up
+        dp = matrix[0]
+        return dp[0]
+
+
+assert Solution.minFallingPathSum([[2, 1, 3], [6, 5, 4], [7, 8, 9]]) == 13
+"""
+Explanation: There are two falling paths with a minimum sum underlined below:
+[[2,1,3],      [[2,1,3],
+ [6,5,4],       [6,5,4],
+ [7,8,9]]       [7,8,9]]
+"""
+assert Solution.minFallingPathSum([[-19, 57], [-40, -5]]) == -59
+"""
+Explanation: The falling path with a minimum sum is underlined below:
+[[-19,57],
+ [-40,-5]]
+"""
+assert Solution.minFallingPathSum(matrix=[[-48]]) == -48
