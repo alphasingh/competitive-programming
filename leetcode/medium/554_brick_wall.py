988538a80de337a1c23e5aa84cff2f55940347a9
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-22 19:24:37 +0530

    AC: 176ms|84% with 19.2MB|62%

--- leetcode/medium/554_brick_wall.py
+++ leetcode/medium/554_brick_wall.py
@@ -6,8 +6,16 @@ https://leetcode.com/problems/brick-wall/
 class Solution:
 
     def leastBricks(self, wall: [[int]]) -> int:
-        bricks_to_be_crossed = len(wall)
-        return bricks_to_be_crossed
+        brick_edge_frequency = dict()
+        for row_of_bricks in wall:
+            brick_edge = 0  # brick row start from 0
+            for brick in row_of_bricks[:-1]:  # exclude last brick
+                brick_edge += brick
+                brick_edge_frequency.setdefault(brick_edge, 0)
+                brick_edge_frequency[brick_edge] += 1
+        # print(brick_edge_frequency)
+        most_frequent_brick_edge = max(brick_edge_frequency.values()) if brick_edge_frequency.values() else 0
+        return len(wall) - most_frequent_brick_edge
 
 
 sol = Solution()
@@ -17,3 +25,5 @@ assert sol.leastBricks([[1, 2, 2, 1],
                         [2, 4],
                         [3, 1, 2],
                         [1, 3, 1, 1]]) == 2
+
+assert sol.leastBricks([[1], [1], [1]]) == 3
