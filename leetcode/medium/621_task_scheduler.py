0586fa4ce6a1bc76d3905f1689258a8dcddfe925
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-22 08:16:09 +0530

    AC: 744ms|9% runtime with 15MB|47% memory

--- leetcode/medium/621_task_scheduler.py
+++ leetcode/medium/621_task_scheduler.py
@@ -4,12 +4,26 @@ https://leetcode.com/problems/task-scheduler/
 
 
 class Solution:
-    alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    alphabets = 'abcdefghijklmnopqrstuvwxyz'.upper()
 
     def leastInterval(self, tasks: [str], n: int) -> int:
         total_time = len(tasks)
-        frequency_of_alphabet = {alphabet: 0 for alphabet in self.alphabets}
-        print(frequency_of_alphabet, n)
+        frequency_of_alphabets = {alphabet: 0 for alphabet in self.alphabets}
+        for task in tasks:
+            frequency_of_alphabets[task] += 1
+        frequency_of_alphabets = list(frequency_of_alphabets.values())
+        max_alphabet_frequency = max(frequency_of_alphabets)
+        # print(frequency_of_alphabets)
+        fillers = (max_alphabet_frequency - 1) * n
+        frequency_of_alphabets.remove(max_alphabet_frequency)
+        # print(frequency_of_alphabets)
+        for frequency_of_alphabet in frequency_of_alphabets:
+            fillers -= min(max_alphabet_frequency - 1, frequency_of_alphabet)
+        # print(fillers)
+        if fillers < 0:
+            fillers = 0
+        total_time += fillers
+        # print('time:', total_time)
         return total_time
 
 
@@ -21,7 +35,7 @@ Explanation:
 A -> B -> idle -> A -> B -> idle -> A -> B
 There is at least 2 units of time between any two same tasks.
 """
-assert sol.leastInterval(tasks=["A", "A", "A", "B", "B", "B"], n=2) == 6
+assert sol.leastInterval(tasks=["A", "A", "A", "B", "B", "B"], n=0) == 6
 """
 Explanation: On this case any permutation of size 6 would work since n = 0.
 ["A","A","A","B","B","B"]
@@ -36,3 +50,4 @@ Explanation:
 One possible solution is
 A -> B -> C -> A -> D -> E -> A -> F -> G -> A -> idle -> idle -> A -> idle -> idle -> A
 """
+assert sol.leastInterval(["A", "C", "C", "C", "C", "A", "A", "B", "B", "B"], n=3) == 13
