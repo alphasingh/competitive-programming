f87590a0ac0aa483163b66d888e3e4f09aff0342
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-26 21:09:15 +0530

    Pass TCs given but failing custom TC

--- leetcode/medium/1642_farthest_building_you_can_reach.py
+++ leetcode/medium/1642_farthest_building_you_can_reach.py
@@ -7,10 +7,25 @@ class Solution:
 
     @staticmethod
     def furthestBuilding(heights: [int], bricks: int, ladders: int) -> int:
-        farthest_building = len(heights) + bricks + ladders
+        farthest_building = 0
+        # print(heights)
+        while farthest_building < len(heights) - 1:
+            height_difference = heights[farthest_building + 1] - heights[farthest_building]
+            # print(height_difference, 'L:', ladders, 'B:', bricks)
+            if height_difference > 0:  # next building is taller
+                if bricks - height_difference >= 0:  # use bricks if possible
+                    bricks -= height_difference
+                elif ladders:  # try to finish ladders first
+                    ladders -= 1
+                else:
+                    break
+            farthest_building += 1
+        # print(farthest_building)
         return farthest_building
 
 
+assert Solution.furthestBuilding(heights=[4, 12, 2, 7, 3, 18, 20, 3, 19], bricks=10, ladders=2) == 7
+assert Solution.furthestBuilding(heights=[14, 3, 19, 3], bricks=17, ladders=0) == 3
 assert Solution.furthestBuilding(heights=[4, 2, 7, 6, 9, 14, 12], bricks=5, ladders=1) == 4
 """
 Explanation: Starting at building 0, you can follow these steps:
@@ -20,5 +35,4 @@ Explanation: Starting at building 0, you can follow these steps:
 - Go to building 4 using your only ladder. You must use either bricks or ladders because 6 < 9.
 It is impossible to go beyond building 4 because you do not have any more bricks or ladders.
 """
-assert Solution.furthestBuilding(heights=[4, 12, 2, 7, 3, 18, 20, 3, 19], bricks=10, ladders=2) == 7
-assert Solution.furthestBuilding(heights=[14, 3, 19, 3], bricks=17, ladders=0) == 3
+assert Solution.furthestBuilding(heights=[4, 14, 2, 7, 3, 18, 20, 3, 19], bricks=10, ladders=2) == 7
