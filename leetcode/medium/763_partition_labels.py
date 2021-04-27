20ddb7a7b8ad0c870ff792e05b46318db7d419b1
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-27 19:20:57 +0530

    Pass given TCs and a custom test case

--- leetcode/medium/763_partition_labels.py
+++ leetcode/medium/763_partition_labels.py
@@ -8,7 +8,24 @@ class Solution:
     @staticmethod
     def partitionLabels(S: str) -> [int]:
         partitions = [-1]
-        return partitions[1:]  # exclude -1
+        indexes = {alphabet: list() for alphabet in set(S)}
+        for index, alphabet in enumerate(S):
+            indexes[alphabet].append(index)
+        # print(indexes)
+        start = end = 0
+        # print(len(S), S)
+        while start <= end < len(S):
+            # print('start', start, 'end', end, 'current', S[start], indexes[S[start]][-1])
+            end = max(end, indexes[S[start]][-1])
+            if start == end:  # partition complete
+                partitions.append(end)
+                # print(partitions)
+                start = end + 1
+                end = start
+            else:
+                start += 1
+        # print(partition_lengths)
+        return [partitions[i + 1] - partitions[i] for i in range(len(partitions) - 1)]
 
 
 assert Solution.partitionLabels(S="ababcbacadefegdehijhklij") == [9, 7, 8]
