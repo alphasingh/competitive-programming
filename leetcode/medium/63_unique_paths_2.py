4357821a55e9418ecf60574baa7b4442e3c06728
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-28 14:19:46 +0530

    Brute force with O(n*n) time and space with dp

--- leetcode/medium/63_unique_paths_2.py
+++ leetcode/medium/63_unique_paths_2.py
@@ -7,9 +7,22 @@ class Solution:
 
     @staticmethod
     def uniquePathsWithObstacles(obstacleGrid: [[int]]) -> int:
-        return obstacleGrid[-1][-1]
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        path = [[1 for _ in range(n)] for _ in range(m)]
+        # decide for first row
+        for c in range(1, n):
+            path[0][c] = path[0][c - 1] if not obstacleGrid[0][c] else 0
+        # decide for first column
+        for r in range(1, m):
+            path[r][0] = path[r - 1][0] if not obstacleGrid[r][0] else 0
+        # decide for each cell with up and left
+        for r in range(1, m):
+            for c in range(1, n):
+                path[r][c] = path[r - 1][c] + path[r][c - 1] if not obstacleGrid[r][c] else 0
+        return path[-1][-1]
 
 
+assert Solution.uniquePathsWithObstacles(obstacleGrid=[[0, 1, 0], [0, 0, 0]]) == 1
 assert Solution.uniquePathsWithObstacles(obstacleGrid=[[0, 0, 0], [0, 1, 0], [0, 0, 0]]) == 2
 """
 Explanation: There is one obstacle in the middle of the 3x3 grid above.
