d9c555d7895ceed88b375a700cac289aa8f74bb9
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-29 07:56:53 +0530

    Add TCs given by leetcode

--- leetcode/easy/88_merge_sorted_array.py
+++ leetcode/easy/88_merge_sorted_array.py
@@ -5,5 +5,9 @@ https://leetcode.com/problems/merge-sorted-array/
 
 class Solution:
     @staticmethod
-    def merge(self, nums1: [int], m: int, nums2: [int], n: int) -> None:
+    def merge(nums1: [int], m: int, nums2: [int], n: int) -> None:
         pass
+
+
+assert Solution.merge(nums1=[1, 2, 3, 0, 0, 0], m=3, nums2=[2, 5, 6], n=3) == [1, 2, 2, 3, 5, 6]
+assert Solution.merge(nums1=[1], m=1, nums2=[], n=0) == [1]
