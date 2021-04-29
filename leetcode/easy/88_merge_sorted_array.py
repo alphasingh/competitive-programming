bc20ab5ae562bb67d7196fd28be6d0535d02a1d1
Abhay Raj Singh <abhayraja4@gmail.com>
2021-04-29 07:55:44 +0530

    88. Merge Sorted Array with solution framework

---
+++ leetcode/easy/88_merge_sorted_array.py
@@ -0,0 +1,9 @@
+"""
+https://leetcode.com/problems/merge-sorted-array/
+"""
+
+
+class Solution:
+    @staticmethod
+    def merge(self, nums1: [int], m: int, nums2: [int], n: int) -> None:
+        pass
