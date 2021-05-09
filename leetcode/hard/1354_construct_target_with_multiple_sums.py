954fac56aafe841a0d7cbcfdfa76954ed39f44ee
Abhay Raj Singh <abhayraja4@gmail.com>
2021-05-09 20:53:28 +0530

    Submit wrong method failing TCs heapify

---
+++ leetcode/hard/1354_construct_target_with_multiple_sums.py
@@ -0,0 +1,38 @@
+"""
+https://leetcode.com/problems/construct-target-array-with-multiple-sums/
+"""
+
+import heapq
+
+
+class Solution:
+    def isPossible(self, target: [int]) -> bool:
+        current_sum = sum(target)
+        ideal = len(target)
+        heapq.heapify(target)  # heapify
+        print(target)
+        while current_sum > ideal:  # reduce sum
+            current_max = heapq.heappop(target)
+            print('max', target, current_max)
+            others = current_sum - current_max
+            to_be_pushed = current_max - others
+            current_sum -= others
+            heapq.heappush(target, to_be_pushed)
+            print(target)
+        return current_sum == ideal
+
+
+sol = Solution()
+assert sol.isPossible([9, 3, 5]) is True
+"""
+Explanation: Start with [1, 1, 1] 
+[1, 1, 1], sum = 3 choose index 1
+[1, 3, 1], sum = 5 choose index 2
+[1, 3, 5], sum = 9 choose index 0
+[9, 3, 5] Done
+"""
+assert sol.isPossible([1, 1, 1, 2]) is False
+"""
+Explanation: Impossible to create target array from [1,1,1,1].
+"""
+assert sol.isPossible([8, 5]) is True
