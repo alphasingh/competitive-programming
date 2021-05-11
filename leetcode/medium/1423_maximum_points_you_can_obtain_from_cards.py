f9ed704e81e5348e12862dece5b12bba5656dbf1
Abhay Raj Singh <abhayraja4@gmail.com>
2021-05-11 14:50:28 +0530

    Submit solution with failing TCs

---
+++ leetcode/medium/1423_maximum_points_you_can_obtain_from_cards.py
@@ -0,0 +1,37 @@
+"""
+https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+"""
+
+
+class Solution:
+
+    @staticmethod
+    def maxScore(cardPoints: [int], k: int) -> int:
+        current_score = sum(cardPoints[k:])
+        max_score = current_score
+        return max_score
+
+
+assert Solution.maxScore(cardPoints=[1, 2, 3, 4, 5, 6, 1], k=3) == 12
+"""
+Explanation: After the first step, your score will always be 1. 
+However, choosing the rightmost card first will maximize your total score.
+The optimal strategy is to take the three cards on the right, 
+giving a final score of 1 + 6 + 5 = 12.
+"""
+assert Solution.maxScore(cardPoints=[2, 2, 2], k=2) == 4
+"""
+Explanation: Regardless of which two cards you take, 
+your score will always be 4.
+"""
+assert Solution.maxScore(cardPoints=[9, 7, 7, 9, 7, 7, 9], k=7) == 55
+"""
+Explanation: You have to take all the cards. 
+Your score is the sum of points of all cards.
+"""
+assert Solution.maxScore(cardPoints=[1, 79, 80, 1, 1, 1, 200, 1], k=3) == 202
+assert Solution.maxScore(cardPoints=[1, 1000, 1], k=1) == 1
+"""
+Explanation: You cannot take the card in the middle. 
+Your best score is 1. 
+"""
