eecaac061192363098b5374160beac297466a82e
Abhay Raj Singh <abhayraja4@gmail.com>
2021-05-15 20:05:04 +0530

    AC with NlogN sorted array and left index

--- leetcode/easy/1365_numbers_smaller_than_current.py
+++ leetcode/easy/1365_numbers_smaller_than_current.py
@@ -5,7 +5,22 @@ https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-numb
 
 class Solution:
     @staticmethod
-    def smallerNumbersThanCurrent(nums: [int]) -> [int]:
+    def smallerNumbersThanCurrent_optimized(nums: [int]) -> [int]:
+        sorted_nums = sorted(nums)
+        total = len(nums)
+        # print(sorted_nums)
+        left_index = {}
+        for index in range(total):
+            if sorted_nums[index] not in left_index:
+                left_index[sorted_nums[index]] = index
+        # print(left_index)
+        for index in range(total):
+            nums[index] = left_index[nums[index]]
+        # print(nums)
+        return nums
+
+    @staticmethod
+    def smallerNumbersThanCurrent(nums: [int]) -> [int]:  # brute force
         total = len(nums)
         smaller = [0] * total
         for index in range(total):
@@ -27,3 +42,8 @@ For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
 """
 assert Solution.smallerNumbersThanCurrent([6, 5, 4, 8]) == [2, 1, 0, 3]
 assert Solution.smallerNumbersThanCurrent([7, 7, 7, 7]) == [0, 0, 0, 0]
+
+# with optimized
+assert Solution.smallerNumbersThanCurrent_optimized([8, 1, 2, 2, 3]) == [4, 0, 1, 1, 3]
+assert Solution.smallerNumbersThanCurrent_optimized([6, 5, 4, 8]) == [2, 1, 0, 3]
+assert Solution.smallerNumbersThanCurrent_optimized([7, 7, 7, 7]) == [0, 0, 0, 0]
