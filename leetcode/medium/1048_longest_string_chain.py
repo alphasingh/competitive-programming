68d6cbc7ab109f7dc06cc5ad1bd5f2d879e975b1
Abhay Raj Singh <abhayraja4@gmail.com>
2021-05-17 20:47:09 +0530

    AC with dfs

--- leetcode/medium/1048_longest_string_chain.py
+++ leetcode/medium/1048_longest_string_chain.py
@@ -6,7 +6,25 @@ https://leetcode.com/problems/longest-string-chain/
 class Solution:
 
     def longestStrChain(self, words: [str]) -> int:
-        return len(words)
+        def dfs(memo, word):
+            if word in memo:
+                return memo[word]
+            max_length = 1
+            for i in range(len(word)):
+                new_word = word[:i] + word[i + 1:]
+                # print(word, newWord)
+                if new_word in words_present:
+                    new_word_length = 1 + dfs(memo, new_word)
+                    max_length = max(max_length, new_word_length)
+            memo[word] = max_length
+            return max_length
+
+        words_present = set(words)
+        memo = {}
+        answer = 0
+        for word in words:
+            answer = max(answer, dfs(memo, word))
+        return answer
 
 
 sol = Solution()
