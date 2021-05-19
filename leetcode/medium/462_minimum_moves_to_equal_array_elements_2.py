5597ee5dc03963a065ce92e612c5941ac63114e5
Abhay Raj Singh <abhayraja4@gmail.com>
2021-05-20 03:46:39 +0530

    Use running sum but fails for custom cases

--- leetcode/medium/462_minimum_moves_to_equal_array_elements_2.py
+++ leetcode/medium/462_minimum_moves_to_equal_array_elements_2.py
@@ -7,11 +7,23 @@ class Solution:
 
     @staticmethod
     def minMoves2(nums: [int]) -> int:
+        n = len(nums)
         nums.sort()  # O(n*log(n))
-        running_moves = minimum_moves = sum(nums)  # O(n)
+        minimum_moves = sum(nums)  # O(n)
+        running_moves = 0
+        left = 0
+        for num in nums:
+            running_moves += num - nums[0]
+        while left < n - 1:
+            gap = nums[left + 1] - nums[left]
+            # print(gap)
+            left += 1
+            running_moves += (gap * left) - (gap * (n - left))
+            minimum_moves = min(minimum_moves, running_moves)
         return minimum_moves
 
 
+assert Solution.minMoves2([1, 2, 6, 10]) == 13
 assert Solution.minMoves2([1, 2, 3]) == 2
 """
 Explanation:
@@ -19,3 +31,4 @@ Only two moves are needed (remember each move increments or decrements one eleme
 [1,2,3]  =>  [2,2,3]  =>  [2,2,2]
 """
 assert Solution.minMoves2([1, 10, 2, 9]) == 16
+assert Solution.minMoves2([1]) == 0
