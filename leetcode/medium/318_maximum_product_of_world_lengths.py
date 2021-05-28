d6041b745683bfc43ba6fb13bff50a0b716fd79e
Abhay Raj Singh <abhayraja4@gmail.com>
2021-05-28 07:06:17 +0530

    Submit failing TCs for structure

---
+++ leetcode/medium/318_maximum_product_of_world_lengths.py
@@ -0,0 +1,12 @@
+"""
+https://leetcode.com/problems/maximum-product-of-word-lengths/
+"""
+
+
+class Solution:
+    @staticmethod
+    def maxProduct(words: [str]) -> int:
+        return 0
+
+
+assert Solution.maxProduct(words=["a", "aa", "aaa", "aaaa"]) == 0
