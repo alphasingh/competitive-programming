f65d9fccaf540303b07e33379493747c97f8a7fe
Abhay Raj Singh <abhayraja4@gmail.com>
2021-05-28 07:31:37 +0530

    Pass given TCs with brute force

--- leetcode/medium/318_maximum_product_of_world_lengths.py
+++ leetcode/medium/318_maximum_product_of_world_lengths.py
@@ -7,6 +7,16 @@ class Solution:
     @staticmethod
     def maxProduct(words: [str]) -> int:
         maximum_product = 0
+        alphabets_in_word = [set(word) for word in words]
+        length_of_word = [len(word) for word in words]
+        # print(alphabets_in_word)
+        number_of_words = len(words)
+        for i in range(number_of_words):
+            for j in range(i + 1, number_of_words):
+                if len(alphabets_in_word[i].intersection(alphabets_in_word[j])) == 0:  # empty set
+                    current_product = length_of_word[i] * length_of_word[j]
+                    maximum_product = max(maximum_product, current_product)
+        # print(maximum_product)
         return maximum_product
 
 
