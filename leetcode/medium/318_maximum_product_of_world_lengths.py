b67e2100439cec92988ad37327b0e54e1663eba2
Abhay Raj Singh <abhayraja4@gmail.com>
2021-05-28 07:23:47 +0530

    Add new TCs to fail existing dummy return

--- leetcode/medium/318_maximum_product_of_world_lengths.py
+++ leetcode/medium/318_maximum_product_of_world_lengths.py
@@ -6,7 +6,13 @@ https://leetcode.com/problems/maximum-product-of-word-lengths/
 class Solution:
     @staticmethod
     def maxProduct(words: [str]) -> int:
-        return 0
+        maximum_product = 0
+        return maximum_product
 
 
 assert Solution.maxProduct(words=["a", "aa", "aaa", "aaaa"]) == 0
+# Explanation: No such pair of words.
+assert Solution.maxProduct(words=["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]) == 16
+# Explanation: The two words can be "abcw", "xtfn".
+assert Solution.maxProduct(words=["a", "ab", "abc", "d", "cd", "bcd", "abcd"]) == 4
+# Explanation: The two words can be "ab", "cd".
