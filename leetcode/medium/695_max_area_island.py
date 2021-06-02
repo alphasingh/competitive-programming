fa53ec7d1dd3b836cda0eec3f41851a43dcf27cd
Abhay Raj Singh <abhayraja4@gmail.com>
2021-06-02 10:14:23 +0530

    dfs for each cell with neighbours

--- leetcode/medium/695_max_area_island.py
+++ leetcode/medium/695_max_area_island.py
@@ -7,8 +7,20 @@ class Solution:
 
     @staticmethod
     def maxAreaOfIsland(grid: [[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+
+        def dfs(i, j):
+            if 0 <= i < m and 0 <= j < n and grid[i][j]:
+                grid[i][j] = 0
+                return 1 + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i + 1, j) + dfs(i, j - 1)
+            return 0
+
         max_area_island = 0
-        max_area_island = min(max_area_island, len(grid))
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col]:
+                    max_area_island = max(max_area_island, dfs(row, col))
         return max_area_island
 
 
