1bebe361f06749ccfbfa52cdcdce79c439acdd53
Abhay Raj Singh <abhayraja4@gmail.com>
2021-06-07 23:01:48 +0530

    Pass current TCs but failing WA logic

--- leetcode/medium/97_interleaving_string.py
+++ leetcode/medium/97_interleaving_string.py
@@ -8,6 +8,19 @@ class Solution:
     @staticmethod
     def isInterleave(s1: str, s2: str, s3: str) -> bool:
         is_s3_possible = False
+        pointer1 = pointer2 = 0
+        length_s1 = len(s1)
+        length_s2 = len(s2)
+        print(length_s1, length_s2)
+        for character in s3:
+            print(character, pointer1, pointer2)
+            if pointer1 < length_s1 and s1[pointer1] == character:
+                pointer1 += 1
+            elif pointer2 < length_s2 and s2[pointer2] == character:
+                pointer2 += 1
+            else:
+                break
+        print(pointer1, pointer2)
         return is_s3_possible
 
 
