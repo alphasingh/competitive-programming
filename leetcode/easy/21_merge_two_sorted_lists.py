dea508968d65cc8b402917e24eb683910b6a1b1b
Abhay Raj Singh <abhayraja4@gmail.com>
2021-06-09 11:30:48 +0530

    Pass TC with only one linked list

--- leetcode/easy/21_merge_two_sorted_lists.py
+++ leetcode/easy/21_merge_two_sorted_lists.py
@@ -9,13 +9,32 @@ class ListNode:
         self.val = val
         self.next = _next
 
+    def __str__(self):
+        return str(self.val) + '>' + str(self.next)
+
 
 class Solution:
     @staticmethod
     def mergeTwoLists(l1, l2):
         if not l1 and not l2:
             return None
-        return ListNode()
+        merged = ListNode()
+        merged_pointer = merged
+        while l1 or l2:
+            if l1 and l2:
+                merged_pointer.next = ListNode(l2.val)
+                l2 = l2.next
+            elif l1:
+                merged_pointer.next = ListNode(l1.val)
+                l1 = l1.next
+            elif l2:
+                merged_pointer.next = ListNode(l2.val)
+                l2 = l2.next
+            merged_pointer = merged_pointer.next
+        print(merged)
+        print(merged.next)
+        return merged.next
 
 
 assert Solution.mergeTwoLists(None, None) is None
+assert str(Solution.mergeTwoLists(None, ListNode(0))) == str(ListNode(0))
