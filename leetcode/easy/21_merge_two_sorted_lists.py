365a5758c9c303f3b716d745f947bde54ebb0d41
Abhay Raj Singh <abhayraja4@gmail.com>
2021-06-09 11:18:55 +0530

    Pass None edge case given by LC

---
+++ leetcode/easy/21_merge_two_sorted_lists.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/merge-two-sorted-lists/
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, _next=None):
+        self.val = val
+        self.next = _next
+
+
+class Solution:
+    @staticmethod
+    def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1 and not l2:
+            return None
+        return ListNode()
+
+
+assert Solution.mergeTwoLists(None, None) is None
