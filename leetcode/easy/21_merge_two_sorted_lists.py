425a7c07204f8e7f81ceaa8b966313b4f153d7c9
Abhay Raj Singh <abhayraja4@gmail.com>
2021-06-09 11:35:36 +0530

    Pass TC with two sorted linked lists

--- leetcode/easy/21_merge_two_sorted_lists.py
+++ leetcode/easy/21_merge_two_sorted_lists.py
@@ -22,8 +22,12 @@ class Solution:
         merged_pointer = merged
         while l1 or l2:
             if l1 and l2:
-                merged_pointer.next = ListNode(l2.val)
-                l2 = l2.next
+                if l1.val < l2.val:
+                    merged_pointer.next = ListNode(l1.val)
+                    l1 = l1.next
+                else:
+                    merged_pointer.next = ListNode(l2.val)
+                    l2 = l2.next
             elif l1:
                 merged_pointer.next = ListNode(l1.val)
                 l1 = l1.next
@@ -31,10 +35,17 @@ class Solution:
                 merged_pointer.next = ListNode(l2.val)
                 l2 = l2.next
             merged_pointer = merged_pointer.next
-        print(merged)
-        print(merged.next)
         return merged.next
 
 
 assert Solution.mergeTwoLists(None, None) is None
 assert str(Solution.mergeTwoLists(None, ListNode(0))) == str(ListNode(0))
+
+example_l1 = ListNode(1, ListNode(2, ListNode(4)))
+example_l2 = ListNode(1, ListNode(3, ListNode(4)))
+example_merged = ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(4))))))
+assert str(Solution.mergeTwoLists(example_l1, example_l2)) == str(example_merged)
+"""
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+"""
