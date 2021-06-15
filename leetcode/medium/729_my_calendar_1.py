19d502a3f2d820e98c16afe9d55e0dac20542260
Abhay Raj Singh <abhayraja4@gmail.com>
2021-06-15 08:20:28 +0530

    Solve TCs by brute force

--- leetcode/medium/729_my_calendar_1.py
+++ leetcode/medium/729_my_calendar_1.py
@@ -12,7 +12,7 @@ class MyCalendar:
     def book(self, start: int, end: int) -> bool:
         can_be_booked = True
         for booking in self.bookings:
-            if booking[0] <= end < booking[1] or booking[0] <= start < booking[1]:
+            if booking[0] < end < booking[1] or booking[0] <= start < booking[1]:
                 can_be_booked = False
                 break
             elif start < booking[0] < end or start < booking[1] < end:
@@ -34,3 +34,15 @@ assert myCalendar.book(15, 25) is False
 # return False, It can not be booked because time 15 is already booked by another event.
 assert myCalendar.book(20, 30) is True
 # return True, The event can be booked, as the first event takes every time less than 20, but not including 20.
+
+myNewCalendar = MyCalendar()
+assert myNewCalendar.book(47, 50) is True
+assert myNewCalendar.book(33, 41) is True
+assert myNewCalendar.book(39, 45) is False
+assert myNewCalendar.book(33, 42) is False
+assert myNewCalendar.book(25, 32) is True
+assert myNewCalendar.book(26, 35) is False
+assert myNewCalendar.book(19, 25) is True
+assert myNewCalendar.book(3, 8) is True
+assert myNewCalendar.book(8, 13) is True
+assert myNewCalendar.book(18, 27) is False
