ca9bff52f68d385d8d0f19aab3fa71e29550c290
Abhay Raj Singh <abhayraja4@gmail.com>
2021-06-22 23:03:17 +0530

    Brute force solves given LC TCs but TLE

--- leetcode/medium/792_number_of_matching_subsequences.py
+++ leetcode/medium/792_number_of_matching_subsequences.py
@@ -5,8 +5,22 @@ https://leetcode.com/problems/number-of-matching-subsequences/
 
 class Solution:
     @staticmethod
+    def subsequence_exists(word: str, string: str):
+        pointer = 0
+        pointer_limit = len(word)
+        for char in string:
+            if char == word[pointer]:
+                pointer += 1
+            if pointer >= pointer_limit:
+                break
+        return pointer_limit == pointer
+
+    @staticmethod
     def numMatchingSubseq(s: str, words: [str]) -> int:
         matches = 0
+        for word in words:
+            if Solution.subsequence_exists(word, s):
+                matches += 1
         return matches
 
 
