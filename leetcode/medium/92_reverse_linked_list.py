46191cb52e54a86879a14e9963a77c8af486ed81
Abhay Raj Singh <abhayraja4@gmail.com>
2021-06-23 14:16:47 +0530

    Reversed list using flat list logic basic

--- leetcode/medium/92_reverse_linked_list.py
+++ leetcode/medium/92_reverse_linked_list.py
@@ -15,9 +15,21 @@ class ListNode:
 
 class Solution:
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
-        return ListNode(1, ListNode(4, ListNode(3, ListNode(2, ListNode(5)))))
+        flat_list = []
+        pointer = head
+        while pointer:
+            flat_list.append(pointer.val)
+            pointer = pointer.next
+        print(flat_list)
+        reversed_ll = ListNode(head.val)  # start
+        left_list = flat_list[:left - 1]
+        reversed_list = flat_list[left - 1:right]
+        right_list = flat_list[right:]
+        print(left_list, reversed_list, right_list)
+        return reversed_ll
 
 
 solution = Solution()
 example_input = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
 assert str(solution.reverseBetween(example_input, 2, 4)) == '1>4>3>2>5>None'
+assert str(solution.reverseBetween(ListNode(5), 1, 1)) == '5>None'
