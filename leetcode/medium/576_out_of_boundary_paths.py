ce7c9417f2d88c967b8056f043a61a0fb211f6c7
Abhay Raj Singh <abhayraja4@gmail.com>
2021-06-25 09:09:25 +0530

    Use hash map as a manual cache

--- leetcode/medium/576_out_of_boundary_paths.py
+++ leetcode/medium/576_out_of_boundary_paths.py
@@ -4,18 +4,24 @@ https://leetcode.com/problems/out-of-boundary-paths/
 
 
 class Solution:
+    cache = {}
+
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-        if maxMove < 0:  # no moves remaining
-            return 0  # no path possible
-        if startRow < 0 or startColumn < 0 or startRow >= m or startColumn >= n:  # boundary reached
-            return 1  # path possible
-        up = self.findPaths(m, n, maxMove - 1, startRow - 1, startColumn)
-        down = self.findPaths(m, n, maxMove - 1, startRow + 1, startColumn)
-        right = self.findPaths(m, n, maxMove - 1, startRow, startColumn + 1)
-        left = self.findPaths(m, n, maxMove - 1, startRow, startColumn - 1)
-        return up + down + left + right  # 4 directions
+        if maxMove < 0:
+            return 0
+        if not (0 <= startRow < m and 0 <= startColumn < n):
+            return 1
+        combination = (m, n, maxMove, startRow, startColumn)
+        if combination not in self.cache:
+            up = self.findPaths(m, n, maxMove - 1, startRow - 1, startColumn)
+            down = self.findPaths(m, n, maxMove - 1, startRow + 1, startColumn)
+            right = self.findPaths(m, n, maxMove - 1, startRow, startColumn + 1)
+            left = self.findPaths(m, n, maxMove - 1, startRow, startColumn - 1)
+            self.cache[combination] = (up + down + right + left) % 1000000007
+        return self.cache[combination]
 
 
 sol = Solution()
 assert sol.findPaths(m=2, n=2, maxMove=2, startRow=0, startColumn=0) == 6
 assert sol.findPaths(m=1, n=3, maxMove=3, startRow=0, startColumn=1) == 12
+assert sol.findPaths(m=8, n=50, maxMove=23, startRow=5, startColumn=26) == 914783380
