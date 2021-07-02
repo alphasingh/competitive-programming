595ab006e1ec48596028104f1a86749292afa612
Abhay Raj Singh <abhayraja4@gmail.com>
2021-07-02 13:25:02 +0530

    Get binary negation

--- leetcode/medium/779_kth_symbol_in_grammar.py
+++ leetcode/medium/779_kth_symbol_in_grammar.py
@@ -6,31 +6,26 @@ https://leetcode.com/problems/k-th-symbol-in-grammar/
 class Solution:
     @staticmethod
     def kthGrammar(n: int, k: int) -> int:
-        kth_symbol = k + n
-        if k % 2 == 0:
-            kth_symbol = 1
-        elif k % 2 != 0:
-            kth_symbol = 0
-        return kth_symbol
+        return bin(k - 1).count('1') & 1
 
 
-assert Solution.kthGrammar(n=1, k=1) == 0
+sol = Solution()
 """
 Explanation: row 1: 0
 """
-assert Solution.kthGrammar(n=2, k=1) == 0
+assert sol.kthGrammar(n=2, k=1) == 0
 """
 Explanation:
 row 1: 0
 row 2: 01
 """
-assert Solution.kthGrammar(n=2, k=2) == 1
+assert sol.kthGrammar(n=2, k=2) == 1
 """
 Explanation:
 row 1: 0
 row 2: 01
 """
-assert Solution.kthGrammar(n=3, k=1) == 0
+assert sol.kthGrammar(n=3, k=1) == 0
 """
 Explanation:
 row 1: 0
