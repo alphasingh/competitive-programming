d42dd66bcb283008489dbbefaa1a9f00570e3d97
Abhay Raj Singh <abhayraja4@gmail.com>
2021-07-03 11:19:26 +0530

    Binary search approach works Lee

--- leetcode/medium/658_find_k_closest_elements.py
+++ leetcode/medium/658_find_k_closest_elements.py
@@ -5,10 +5,27 @@ https://leetcode.com/problems/find-k-closest-elements/
 
 class Solution:
     @staticmethod
+    def findClosestElements_binary(A, k, x):
+        left, right = 0, len(A) - k
+        while left < right:
+            mid = (left + right) // 2
+            if x - A[mid] > A[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+        return A[left:left + k]
+
+    @staticmethod
     def findClosestElements(arr: [int], k: int, x: int) -> [int]:
-        closest = arr.copy()
+        optimal_start = 0
+        optimal_sum = sum(arr[optimal_start:optimal_start + k])
+        print(optimal_sum)
+        n = len(arr)
+        for i in range(n - k):
+            pass
         if x != k:
-            closest = [1, 2, 3, 4]
+            optimal_start = 0
+        closest = arr[optimal_start:optimal_start + k]
         return closest
 
 
