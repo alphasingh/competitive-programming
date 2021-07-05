6ad89010ee567fb726392657fea0e7b7afdce39f
Abhay Raj Singh <abhayraja4@gmail.com>
2021-07-05 13:24:25 +0530

    Submit correct brute force O(m*n) with extra space

--- leetcode/easy/566_reshape_the_matrix.py
+++ leetcode/easy/566_reshape_the_matrix.py
@@ -8,9 +8,17 @@ class Solution:
     def matrixReshape(mat: [[int]], r: int, c: int) -> [[int]]:
         m, n = len(mat), len(mat[0])
         if m * n == r * c:  # reshape possible
-            mat = []  # fill elements
-        if r == 1:
-            mat = [[1, 2, 3, 4]]
+            elements = []
+            elements_pointer = 0
+            for i in range(m):
+                for j in range(n):
+                    elements.append(mat[i][j])
+            mat = [[0] * c for _ in range(r)]
+            for i in range(r):
+                for j in range(c):
+                    mat[i][j] = elements[elements_pointer]
+                    elements_pointer += 1
+        # print(mat)
         return mat
 
 
