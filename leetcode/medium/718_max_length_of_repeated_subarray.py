213b8452a987f9585a767027ca690b9fffa9704d
Abhay Raj Singh <abhayraja4@gmail.com>
2021-07-09 10:21:30 +0530

    Brute force but TLE

--- leetcode/medium/718_max_length_of_repeated_subarray.py
+++ leetcode/medium/718_max_length_of_repeated_subarray.py
@@ -5,7 +5,7 @@ https://leetcode.com/problems/maximum-length-of-repeated-subarray/
 
 class Solution:
     @staticmethod
-    def findLength(nums1: [int], nums2: [int]) -> int:
+    def helper(nums1: [int], nums2: [int]) -> int:
         max_common_length = 0
         nums1_length, nums2_length = len(nums1), len(nums2)
         for start in range(nums1_length):  # traverse nums1
@@ -19,13 +19,19 @@ class Solution:
                     # print(nums1[pointer1], pointer1, nums2[pointer2], pointer2)
                     current_max += 1
                     pointer1 += 1
+                    max_common_length = max(max_common_length, current_max)
                 else:
                     pointer1 = start
                     if current_max > 0:
                         pointer2 -= 1
                         current_max = 0
-                max_common_length = max(max_common_length, current_max)
+                        max_common_length = max(max_common_length, current_max)
         # print(max_common_length)
+        return max_common_length
+
+    @staticmethod
+    def findLength(nums1: [int], nums2: [int]) -> int:
+        max_common_length = max(Solution.helper(nums1, nums2), Solution.helper(nums2, nums1))
         return max_common_length
 
 
@@ -35,3 +41,4 @@ assert Solution.findLength(nums1=[0, 0, 0, 0, 0], nums2=[0, 0, 0, 0, 0]) == 5
 assert Solution.findLength(nums1=[1, 2, 3, 2, 1], nums2=[3, 2, 1, 4, 7, 1, 2, 3, 2]) == 4
 assert Solution.findLength(nums1=[1, 2, 3, 2, 0, 4, 7, 1, 2, 3], nums2=[3, 2, 1, 4, 7, 1, 2, 3, 2]) == 5
 assert Solution.findLength([0, 1, 1, 1, 0], [1, 1, 1, 1, 1]) == 3
+assert Solution.findLength([0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]) == 9
