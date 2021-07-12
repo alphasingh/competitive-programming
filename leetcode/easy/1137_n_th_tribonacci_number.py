e95c6230b5c4184f80934291841530085a8f8cba
Abhay Raj Singh <abhayraja4@gmail.com>
2021-07-12 11:17:28 +0530

    Using only O(1) space and fibonacci approach

--- leetcode/easy/1137_n_th_tribonacci_number.py
+++ leetcode/easy/1137_n_th_tribonacci_number.py
@@ -6,15 +6,22 @@ https://leetcode.com/problems/n-th-tribonacci-number/
 class Solution:
     cache = {}
 
-    def tribonacci(self, n: int) -> int:
+    def tribonacci_cache(self, n: int) -> int:
         if n == 0:
             return 0
         elif n < 3:
             return 1
         if n not in self.cache:
-            tri = self.tribonacci(n - 3) + self.tribonacci(n - 2) + self.tribonacci(n - 1)
+            tri = self.tribonacci_cache(n - 3) + self.tribonacci_cache(n - 2) + self.tribonacci_cache(n - 1)
             self.cache[n] = tri
         return self.cache[n]
+
+    @staticmethod
+    def tribonacci(n: int) -> int:
+        a, b, c = 1, 0, 0
+        for _ in range(n):
+            a, b, c = b, c, a + b + c
+        return c
 
 
 sol = Solution()
