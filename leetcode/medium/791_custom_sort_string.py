2a3049b228026ec705c971bda283ab62b5431a47
Abhay Raj Singh <abhayraja4@gmail.com>
2021-07-15 11:21:27 +0530

    Pass given TC with actual logic

--- leetcode/medium/791_custom_sort_string.py
+++ leetcode/medium/791_custom_sort_string.py
@@ -4,13 +4,27 @@ https://leetcode.com/problems/custom-sort-string/
 
 
 class Solution:
-    @staticmethod
-    def customSortString(order: str, string: str) -> str:
-        ordered = order + string
-        return "cbad"
+    ALPHABETS = 'abcdefghijklmnopqrstuvwxyz'
+
+    def customSortString(self, order: str, string: str) -> str:
+        ordered = ''
+        string_counter = {alphabet: 0 for alphabet in self.ALPHABETS}
+        for ch in string:
+            string_counter[ch] += 1
+        string_set = set(string)
+        order_set = set(order)
+        # print(order_set, string_set)
+        string_set_not_in_order_set = string_set.difference(order_set)
+        # print(string_set_not_in_order_set)
+        # print(string_counter)
+        ordered += ''.join([alphabet * string_counter[alphabet] for alphabet in order])
+        ordered += ''.join([alphabet * string_counter[alphabet] for alphabet in string_set_not_in_order_set])
+        # print(ordered)
+        return ordered
 
 
-assert Solution.customSortString("cba", "abcd") in ["cbad", "dcba", "cdba", "cbda"]
+sol = Solution()
+assert sol.customSortString("cba", "abcd") in ["cbad", "dcba", "cdba", "cbda"]
 """
 Explanation: 
 "a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a". 
