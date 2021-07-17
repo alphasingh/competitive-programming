e417731d76bec5dee2bbf84251cff2c68c2edf7c
Abhay Raj Singh <abhayraja4@gmail.com>
2021-07-17 08:20:14 +0530

    18. use binary search with 2 pointer tactic

--- leetcode/medium/18_4sum.py
+++ leetcode/medium/18_4sum.py
@@ -2,23 +2,28 @@
 https://leetcode.com/problems/4sum-ii/
 """
 
-from itertools import product
-from collections import Counter
+import bisect
 
 
 class Solution:
 
     @staticmethod
     def fourSum(nums: [int], target: int) -> [[int]]:
+        nums.sort()
         n, count = len(nums), 0
         combinations = []
         for a in range(n):
             for b in range(a + 1, n):
                 for c in range(b + 1, n):
-                    for d in range(c + 1, n):
-                        if nums[a] + nums[b] + nums[c] + nums[d] == target:
-                            combinations.append(tuple(sorted((nums[a], nums[b], nums[c], nums[d]))))
-                            count += 1
+                    abc = nums[a] + nums[b] + nums[c]
+                    rem = target - abc
+                    index_l = bisect.bisect_left(nums, rem)
+                    index_r = bisect.bisect_right(nums, rem)
+                    # print(index_l, index_r)
+                    if n > index_l != index_r and index_r > c and index_l != a and index_l != b and index_l != c and \
+                            nums[index_l] == rem:
+                        combinations.append(tuple(sorted((nums[a], nums[b], nums[c], rem))))
+                        count += 1
         # print(count)
         # print(set(combinations))
         combinations = list(list(combo) for combo in set(combinations))
@@ -27,5 +32,5 @@ class Solution:
 
 
 assert sorted(Solution.fourSum(nums=[2, 2, 2, 2, 2], target=8)) == [[2, 2, 2, 2]]
-assert sorted(Solution.fourSum([-5, 5, 4, -3, 0, 0, 4, -2], 4)) == [[-5, 0, 4, 5], [-3, -2, 4, 5]]
 assert sorted(Solution.fourSum(nums=[1, 0, -1, 0, -2, 2], target=0)) == [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
+assert sorted(Solution.fourSum([-5, 5, 4, -3, 0, 0, 4, -2], 4)) == [[-5, 0, 4, 5], [-3, -2, 4, 5]]
