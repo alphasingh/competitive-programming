d4a851d4f3a1c5fbafce7b33c5eba012d4df99fc
Abhay Raj Singh <abhayraja4@gmail.com>
2021-08-01 07:38:53 -0400

    827. Debug values and add TC

--- leetcode/hard/827_making_Large_island.py
+++ leetcode/hard/827_making_Large_island.py
@@ -8,31 +8,41 @@ class Solution:
 
     def largestIsland(self, grid: [[int]]) -> int:
         def dfs(xi, yi, ind):
-            area = 1
+            area = 0
             grid[xi][yi] = ind
             for i, j in move(xi, yi):
                 if grid[i][j] == 1:
                     area += dfs(i, j, ind)
-            return area
+            return area + 1
 
         def move(a, b):
-            for i, j in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-                if 0 <= a + i < n and 0 <= b + j < n:
-                    yield a + i, b + j
+            for di, dj in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                if 0 <= a + di < n and 0 <= b + dj < n:
+                    yield a + di, b + dj
 
         n = len(grid)
+        # print(grid)
+        self.areas = {0: 0}
         index = 2
         for x in range(n):
             for y in range(n):
-                self.areas[index] = dfs(x, y, index)
-                index += 1
+                if grid[x][y] == 1:
+                    self.areas[index] = dfs(x, y, index)
+                    index += 1
 
-        print(self.areas.values())
+        # print(self.areas.values())
         res = max(self.areas.values())
         for r in range(n):
             for c in range(n):
                 if grid[r][c] == 0:  # can work as connector
                     possible = set(grid[mr][mc] for mr, mc in move(r, c))
                     res = max(res, sum(self.areas[index] for index in possible) + 1)
-        self.areas = {0: 0}
+        # print(grid)
+        # print(res)
         return res
+
+
+sol = Solution()
+
+assert sol.largestIsland(grid=[[1, 0], [0, 1]]) == 3
+# Explanation: Change one 0 to 1 and connect two 1s, then we get an island with area = 3.
