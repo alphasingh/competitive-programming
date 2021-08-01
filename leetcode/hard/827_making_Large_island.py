4b6b3fe87cd884e62b32e1f6cd25a19fd9a36b6e
Abhay Raj Singh <abhayraja4@gmail.com>
2021-08-01 07:30:42 -0400

    827. Use DFS and keep connector logic

--- leetcode/hard/827_making_Large_island.py
+++ leetcode/hard/827_making_Large_island.py
@@ -7,16 +7,32 @@ class Solution:
     areas = {0: 0}
 
     def largestIsland(self, grid: [[int]]) -> int:
-        def dfs():
-            pass
+        def dfs(xi, yi, ind):
+            area = 1
+            grid[xi][yi] = ind
+            for i, j in move(xi, yi):
+                if grid[i][j] == 1:
+                    area += dfs(i, j, ind)
+            return area
 
         def move(a, b):
-            return a + b
+            for i, j in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                if 0 <= a + i < n and 0 <= b + j < n:
+                    yield a + i, b + j
 
         n = len(grid)
+        index = 2
         for x in range(n):
             for y in range(n):
-                dfs()
-                move(x, y)
+                self.areas[index] = dfs(x, y, index)
+                index += 1
+
+        print(self.areas.values())
+        res = max(self.areas.values())
+        for r in range(n):
+            for c in range(n):
+                if grid[r][c] == 0:  # can work as connector
+                    possible = set(grid[mr][mc] for mr, mc in move(r, c))
+                    res = max(res, sum(self.areas[index] for index in possible) + 1)
         self.areas = {0: 0}
-        return len(grid)
+        return res
