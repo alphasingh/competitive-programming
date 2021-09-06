c906b64d1bf9bc5e1149e4d45bd6ef1b41886374
Abhay Raj Singh <abhayraja4@gmail.com>
2021-09-06 15:26:25 -0400

    1629. pass with actual logic using O(1) space

--- leetcode/easy/1629_slowest_key.py
+++ leetcode/easy/1629_slowest_key.py
@@ -6,7 +6,17 @@ https://leetcode.com/problems/slowest-key/
 class Solution:
     @staticmethod
     def slowestKey(releaseTimes: [int], keysPressed: str) -> str:
-        return "c"
+        max_duration = releaseTimes[0]
+        slowest_key = keysPressed[0]
+        n = len(keysPressed)
+        for i in range(1, n):
+            duration = releaseTimes[i] - releaseTimes[i - 1]
+            if duration == max_duration:
+                slowest_key = max(slowest_key, keysPressed[i])
+            elif duration > max_duration:
+                slowest_key = keysPressed[i]
+                max_duration = duration
+        return slowest_key
 
 
 assert Solution.slowestKey(releaseTimes=[9, 29, 49, 50], keysPressed="cbcd") == "c"
@@ -24,4 +34,17 @@ Keypress for 'd' had a duration of 50 - 49 = 1
 The longest of these was the keypress for 'b' and the second keypress for 'c', 
 both with duration 20.
 'c' is lexicographically larger than 'b', so the answer is 'c'.
+"""
+
+assert Solution.slowestKey(releaseTimes=[12, 23, 36, 46, 62], keysPressed="spuda") == "a"
+"""
+Input: releaseTimes = [12,23,36,46,62], keysPressed = "spuda"
+Output: "a"
+Explanation: The keypresses were as follows:
+Keypress for 's' had a duration of 12.
+Keypress for 'p' had a duration of 23 - 12 = 11.
+Keypress for 'u' had a duration of 36 - 23 = 13.
+Keypress for 'd' had a duration of 46 - 36 = 10.
+Keypress for 'a' had a duration of 62 - 46 = 16.
+The longest of these was the keypress for 'a' with duration 16.
 """
