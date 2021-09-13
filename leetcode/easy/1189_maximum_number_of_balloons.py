43e367d1324c3ddb65e6b7e94f1c81971129935e
Abhay Raj Singh <abhayraja4@gmail.com>
2021-09-13 07:53:48 -0400

    1189. Use frequency of balloon string

--- leetcode/easy/1189_maximum_number_of_balloons.py
+++ leetcode/easy/1189_maximum_number_of_balloons.py
@@ -6,8 +6,12 @@ https://leetcode.com/problems/maximum-number-of-balloons/
 class Solution:
     @staticmethod
     def maxNumberOfBalloons(text: str) -> int:
-        balloons = 0
-        balloons += len(text)
+        f = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
+        for character in text:
+            if character in f:
+                f[character] += 1
+        # print(f)
+        balloons = min(f['b'], f['a'], f['l'] // 2, f['o'] // 2, f['n'])
         return balloons
 
 
