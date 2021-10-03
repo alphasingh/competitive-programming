e4abec7874ec77d34077026dcde7eda6bbd756e7
Abhay Raj Singh <abhayraja4@gmail.com>
2021-10-03 01:24:42 -0400

    947. use bfs logic and keep updating visited

--- leetcode/medium/947_most_stones_removed.py
+++ leetcode/medium/947_most_stones_removed.py
@@ -8,12 +8,28 @@ class Solution:
     def removeStones(stones: [[int]]) -> int:
         removed = 0
         total_stones = len(stones)
-        if total_stones == 6:
-            removed = 5
-        elif total_stones == 5:
-            removed = 3
-        elif total_stones == 1:
-            removed = 0
+        common_stones = [False] * total_stones
+        visited_stones = [False] * total_stones
+
+        def checkShared(index) -> None:
+            _stone = stones[index]
+            visited_stones[index] = True
+            for _stone_i in range(total_stones):
+                __stone = stones[_stone_i]
+                share_common_row = _stone[1] == __stone[1]
+                share_common_col = _stone[0] == __stone[0]
+                if not visited_stones[_stone_i] and (share_common_row or share_common_col) and (_stone != __stone):
+                    common_stones[_stone_i] = True
+                    checkShared(_stone_i)
+
+        for stone_i in range(total_stones):
+            if not visited_stones[stone_i]:
+                checkShared(stone_i)
+        # print(common_stones)
+        # print(visited_stones)
+        for common_stone in common_stones:
+            if common_stone:
+                removed += 1
         return removed
 
 
