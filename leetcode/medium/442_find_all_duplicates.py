a8ca3bcb424ea803ce2ca660d124627f26a8addd
Abhay Raj Singh <abhayraja4@gmail.com>
2021-10-06 16:38:42 -0400

    442. rename variables for clarity

--- leetcode/medium/442_find_all_duplicates.py
+++ leetcode/medium/442_find_all_duplicates.py
@@ -5,37 +5,22 @@ https://leetcode.com/problems/find-all-duplicates-in-an-array/
 
 class Solution:
 
-    def findDuplicates(self, nums: [int]) -> [int]:
+    @staticmethod
+    def findDuplicates(nums: [int]) -> [int]:
         length = len(nums)
-        # print(nums)
-        pointer = 0
-        _i = 0
+        iterator = 0  # will increment sequentially
+        jumper = 0  # can jump depending on nums[i]
 
-        while pointer < length:
-            if nums[_i] <= length:  # unvisited
-                temp = nums[_i]
-                nums[_i] = length + 1
+        while iterator < length:
+            if nums[jumper] <= length:  # unvisited
+                temp = nums[jumper]
+                nums[jumper] = length + 1
                 if temp <= length:
-                    _i = temp - 1
+                    jumper = temp - 1
             else:  # visited
-                pointer += 1
-                nums[_i] += 1
-                _i = pointer
-            # index = nums[_i] % 7 - 1
-            # if nums[index] <= length:
-            #     nums[index] = length
-            # nums[index] += 1
-            #
-            # self.pointer += 1
-
-        # while pointer < length:
-        #     update(pointer)
-        #     ahead = nums[pointer]
-        #     update(ahead)
-        #     pointer += 1
-
-        # print(nums)
-        # indices = set()
+                iterator += 1
+                nums[jumper] += 1
+                jumper = iterator
         for i in range(length):
             if nums[i] - length == 3:  # 1 extra
                 yield i + 1
