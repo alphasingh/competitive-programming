04b5607b4bb07451fa37ef1f6c5e36da81d4dd85
Abhay Raj Singh <abhayraja4@gmail.com>
2021-10-22 12:17:30 -0400

    add Test cases

--- leetcode/medium/994_rotten_oranges.py
+++ leetcode/medium/994_rotten_oranges.py
@@ -1,17 +1,17 @@
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
+    @staticmethod
+    def orangesRotting(grid: [[int]]) -> int:
         minutes = 0
         m = len(grid)
         n = len(grid[0])
-        
+
         def anyRottenNeighbor(r, c) -> bool:
-            up = r > 0 and grid[r-1][c] == 2
-            down = r < m-1 and grid[r+1][c] == 2
-            right = c > 0 and grid[r][c-1] == 2
-            left = c < n-1 and grid[r][c+1] == 2
+            up = r > 0 and grid[r - 1][c] == 2
+            down = r < m - 1 and grid[r + 1][c] == 2
+            right = c > 0 and grid[r][c - 1] == 2
+            left = c < n - 1 and grid[r][c + 1] == 2
             return up or down or right or left
-            
-        
+
         while True:
             rot_next = []
             for i in range(m):
@@ -27,7 +27,7 @@ class Solution:
                 i, j = to_be_rotten[0], to_be_rotten[1]
                 grid[i][j] = 2
             minutes += 1
-            
+
         # count all fresh oranges
         fresh = 0
         for i in range(m):
@@ -35,3 +35,15 @@ class Solution:
                 if grid[i][j] == 1:
                     fresh += 1
         return minutes if fresh == 0 else -1
+
+
+assert Solution.orangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1]]) == 4
+assert Solution.orangesRotting([[2, 1, 1], [0, 1, 1], [1, 0, 1]]) == -1
+"""
+Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, 
+because rotting only happens 4-directionally.
+"""
+assert Solution.orangesRotting([[0, 2]]) == 0
+"""
+Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+"""
