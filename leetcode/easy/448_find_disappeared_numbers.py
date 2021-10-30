d30cdac971ed84c20fcb6f64def2dcf9cbe45132
Abhay Raj Singh <abhayraja4@gmail.com>
2021-10-30 05:37:35 -0400

    448. reduce lines of code simply with negating

--- leetcode/easy/448_find_disappeared_numbers.py
+++ leetcode/easy/448_find_disappeared_numbers.py
@@ -6,16 +6,10 @@ https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 class Solution:
     @staticmethod
     def findDisappearedNumbers(nums: [int]) -> [int]:
-        disappeared = []
-        n = len(nums)
-        for i in range(n):
-            index = (nums[i] - 1) % n
-            nums[index] += n
-            # print(nums)
-        for i in range(n):
-            if nums[i] <= n:
-                disappeared.append(i + 1)
-        return disappeared
+        for num in nums:
+            index = abs(num) - 1
+            nums[index] = -abs(nums[index])
+        return [i + 1 for i, num in enumerate(nums) if num > 0]
 
 
 assert Solution.findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]) == [5, 6]
