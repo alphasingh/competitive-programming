697dc54ed7607c826af6963cd4b9afddceca359a
Abhay Raj Singh <abhayraja4@gmail.com>
2021-11-29 13:54:05 -0500

    721. Union set theory for merged results

--- leetcode/medium/721_accounts_merge.py
+++ leetcode/medium/721_accounts_merge.py
@@ -8,17 +8,28 @@ class Solution:
     def accountsMerge(accounts: [[str]]) -> [[str]]:
         total_accounts = len(accounts)
         accounts_to_be_merged = {account: set() for account in range(total_accounts)}
-        print(accounts_to_be_merged)
+        merged_accounts = []
         for i in range(total_accounts):
             common = set()
             for j in range(total_accounts):
                 if len(set(accounts[i]).intersection(set(accounts[j]))) > 1:
                     common.add(j)
             accounts_to_be_merged[i] = common
-        print(accounts_to_be_merged)
         for i in range(total_accounts):
-            accounts[i] = sorted(accounts[i])
-        return sorted(accounts)
+            current_merge = accounts_to_be_merged[i]
+            if not current_merge:
+                continue
+            current_emails = set(accounts[i][1:])
+            # print(current_emails)
+            for a in accounts_to_be_merged[i]:
+                current_emails = current_emails.union(set(accounts[a][1:]))
+                accounts_to_be_merged[a] = None
+            # print(current_emails)
+            merged = [accounts[i][0]] + sorted(current_emails)
+            merged_accounts.append(merged)
+        # print(merged_accounts)
+        # print(accounts_to_be_merged)
+        return merged_accounts
 
 
 sample_accounts = [["Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"],
@@ -27,10 +38,10 @@ sample_accounts = [["Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"],
                    ["Hanzo", "Hanzo3@m.co", "Hanzo1@m.co", "Hanzo0@m.co"],
                    ["Fern", "Fern5@m.co", "Fern1@m.co", "Fern0@m.co"]]
 sample_merged = [["Ethan", "Ethan0@m.co", "Ethan4@m.co", "Ethan5@m.co"],
+                 ["Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"],
                  ["Gabe", "Gabe0@m.co", "Gabe1@m.co", "Gabe3@m.co"],
                  ["Hanzo", "Hanzo0@m.co", "Hanzo1@m.co", "Hanzo3@m.co"],
-                 ["Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"],
-                 ["Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"]]
+                 ["Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"]]
 assert sorted(Solution.accountsMerge(sample_accounts)) == sorted(sample_merged)
 
 sample_accounts = [["John", "johnsmith@mail.com", "john_newyork@mail.com"],
