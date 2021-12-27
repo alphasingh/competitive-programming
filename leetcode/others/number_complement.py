b6546e9785ca4b9d77898a06d63c6883609c6408
Abhay Raj Singh <abhayraja4@gmail.com>
2021-12-27 11:01:05 -0500

    476. replace string 3 vars [Ranking: 74688]

--- leetcode/others/number_complement.py
+++ leetcode/others/number_complement.py
@@ -7,21 +7,19 @@ https://leetcode.com/problems/number-complement/
 class Solution:
     @staticmethod
     def findComplement(num: int) -> int:
-        number_in_binary = bin(num)
-        all_bits_set_in_binary = '1' * (len(number_in_binary) - 2)
-        set_number_in_decimal = int(all_bits_set_in_binary, 2)
-        return set_number_in_decimal - num
+        b = bin(num)[2:].replace('1', '2')
+        b = b.replace('0', '1')
+        b = b.replace('2', '0')
+        return int(b, 2)
 
 
-print(Solution.findComplement(num=5))
-print(Solution.findComplement(num=1))
-
+assert Solution.findComplement(5) == 2
 """
-Input: num = 5
-Output: 2
-Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
-
-Input: num = 1
-Output: 0
-Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+Explanation: The binary representation of 5 is 101 (no leading zero bits), 
+and its complement is 010. So you need to output 2.
+"""
+assert Solution.findComplement(1) == 0
+"""
+Explanation: The binary representation of 1 is 1 (no leading zero bits), 
+and its complement is 0. So you need to output 0.
 """
