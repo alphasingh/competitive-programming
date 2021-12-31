b09f0b7c45a7c6fb8994bff43373f5a33044ee29
Abhay Raj Singh <abhayraja4@gmail.com>
2021-12-31 14:45:58 -0500

    1337. sort with 2 keys [Ranking: 70970]

---
+++ leetcode/easy/1337_k_weakest_rows_in_matrix.py
@@ -0,0 +1,47 @@
+"""
+https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+"""
+
+
+class Solution:
+    @staticmethod
+    def kWeakestRows(mat: [[int]], k: int) -> [int]:
+        n = len(mat)
+        rows = [[mat[i].count(1), i] for i in range(n)]
+        # print(rows)
+        rows.sort(key=lambda x: (x[0], x[1]))
+        return [row[1] for row in rows[:k]]
+
+
+assert Solution.kWeakestRows(mat=
+                             [[1, 1, 0, 0, 0],
+                              [1, 1, 1, 1, 0],
+                              [1, 0, 0, 0, 0],
+                              [1, 1, 0, 0, 0],
+                              [1, 1, 1, 1, 1]],
+                             k=3) == [2, 0, 3]
+"""
+Explanation: 
+The number of soldiers in each row is: 
+- Row 0: 2 
+- Row 1: 4 
+- Row 2: 1 
+- Row 3: 2 
+- Row 4: 5 
+The rows ordered from weakest to strongest are [2,0,3,1,4].
+"""
+assert Solution.kWeakestRows(mat=
+                             [[1, 0, 0, 0],
+                              [1, 1, 1, 1],
+                              [1, 0, 0, 0],
+                              [1, 0, 0, 0]],
+                             k=2) == [0, 2]
+"""
+Explanation: 
+The number of soldiers in each row is: 
+- Row 0: 1 
+- Row 1: 4 
+- Row 2: 1 
+- Row 3: 1 
+The rows ordered from weakest to strongest are [0,2,3,1].
+"""
